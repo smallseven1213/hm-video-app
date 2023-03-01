e805e73bc2acbe4810cce7780050df3f9bb03513
Cyril <smallseven@gmail.com>
2023-03-01 10:49:33 +0800

    splash,ad不用路由控制
    splash,ad不用路由控制

--- packages/shared/lib/widgets/ad.dart
+++ packages/shared/lib/widgets/ad.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 
 class Ad extends StatelessWidget {
+  // onNext
+  final void Function() onNext;
+
   const Ad({
     Key? key,
+    required this.onNext,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Ad Screen'),
+        child: // button ,and click it will call onNext
+            TextButton(
+                onPressed: () {
+                  onNext();
+                },
+                child: const Text('go to home')),
       ),
     );
   }
