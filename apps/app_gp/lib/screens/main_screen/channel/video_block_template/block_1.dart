e6c7e5ad7d19f0c490cf6cf18e56b995479eab5c
martina <iowoiy.yo@gmail.com>
2023-03-17 15:09:57 +0800

    feat: block1
    feat: block1

--- apps/app_gp/lib/screens/main_screen/channel/video_block_template/block_1.dart
+++ apps/app_gp/lib/screens/main_screen/channel/video_block_template/block_1.dart
@@ -1,38 +1,67 @@
+import 'package:app_gp/widgets/video_block_grid_view_row.dart';
 import 'package:app_gp/widgets/video_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:shared/models/channel_info.dart';
-import 'package:shared/widgets/grid_silver_fix_size_delegate.dart';
 
-import '../../../../widgets/video_block_grid_view.dart';
+List<List<Data>> organizeRowData(List videos, Blocks block) {
+  List<List<Data>> result = [];
+  int blockQuantity = block.quantity ?? 0;
+  int blockLength = block.isAreaAds == true ? 6 : 5;
+
+  for (int i = 0; i < blockQuantity;) {
+    if (i % blockLength == 0 || i % blockLength == 5) {
+      result.add([videos[i]]);
+      i++;
+    } else {
+      if (i + 2 > videos.length) {
+        result.add([videos[i]]);
+        i++;
+      } else {
+        result.add([videos[i], videos[i + 1]]);
+        i += 2;
+      }
+    }
+  }
+  return result;
+}
 
 // 一大多小
 class Block1Widget extends StatelessWidget {
-  List<Data> videos = [];
-  Block1Widget({Key? key, required this.videos}) : super(key: key);
+  Blocks block;
+  Block1Widget({
+    Key? key,
+    required this.block,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // 除了第0個的物件
-    List<Data> videosFromSecond = videos.sublist(1);
-    // 取得this.videos的第一個物件
-    Data videoFromFirst = videos[0];
-
+    List<Data> videos = block.videos?.data ?? [];
+    List<List<Data>> result = organizeRowData(videos, block);
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
-        children: [
-          VideoPreviewWidget(
-            title: videoFromFirst.title ?? '',
-            tags: videoFromFirst.tags ?? [],
-            timeLength: videoFromFirst.timeLength ?? 0,
-            coverHorizontal: videoFromFirst.coverHorizontal ?? '',
-            coverVertical: videoFromFirst.coverVertical ?? '',
-            videoViewTimes: videoFromFirst.videoViewTimes ?? 0,
-            imageRatio: 374 / 198,
-          ),
-          const SizedBox(height: 10),
-          VideoBlockGridView(videos: videosFromSecond),
-        ],
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: List.generate(
+          result.length,
+          (index) {
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Container(
+                child: index % 4 == 0 || index % 4 == 3
+                    ? VideoPreviewWidget(
+                        title: result[index][0].title ?? '',
+                        tags: result[index][0].tags ?? [],
+                        timeLength: result[index][0].timeLength ?? 0,
+                        coverHorizontal: result[index][0].coverHorizontal ?? '',
+                        coverVertical: result[index][0].coverVertical ?? '',
+                        videoViewTimes: result[index][0].videoViewTimes ?? 0,
+                        imageRatio: 374 / 198,
+                      )
+                    : VideoBlockGridViewRow(videoData: result[index]),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
