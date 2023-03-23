1aee294090fde424e2f49e751c1f9d6acb1d869e
martina <iowoiy.yo@gmail.com>
2023-03-23 10:49:07 +0800

    fix:  error

--- apps/app_gp/lib/screens/main_screen/channel/video_block_template/block_6.dart
+++ apps/app_gp/lib/screens/main_screen/channel/video_block_template/block_6.dart
@@ -1,11 +1,10 @@
-// Block6Widget
-
+// // Block6Widget
 import 'package:app_gp/widgets/video_block_footer.dart';
 import 'package:app_gp/widgets/video_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:shared/models/channel_info.dart';
-import 'package:shared/models/index.dart';
 
+// 六小
 class Block6Widget extends StatelessWidget {
   final Blocks block;
   const Block6Widget({
@@ -16,34 +15,44 @@ class Block6Widget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<Data> videos = block.videos?.data ?? [];
-
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      padding: const EdgeInsets.all(16.0),
-      child: Center(
-        child: Row(children: [
-          ...List.generate(
-            videos.length,
-            (index) => Container(
-              padding: const EdgeInsets.only(right: 8.0),
-              width: (MediaQuery.of(context).size.width - 16) * 0.7,
-              height:
-                  (MediaQuery.of(context).size.width - 16) * 0.7 / 16 * 9 + 40,
-              child: VideoPreviewWidget(
-                title: videos[index].title ?? '',
-                tags: videos[index].tags ?? [],
-                timeLength: videos[index].timeLength ?? 0,
-                coverHorizontal: videos[index].coverHorizontal ?? '',
-                coverVertical: videos[index].coverVertical ?? '',
-                videoViewTimes: videos[index].videoViewTimes ?? 0,
-                detail: videos[index],
-                imageRatio: 143 / 80,
-                isEmbeddedAds: block.isEmbeddedAds ?? false,
-              ),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Center(
+              child: Row(children: [
+                ...List.generate(
+                  videos.length,
+                  (index) => Container(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    width: (MediaQuery.of(context).size.width - 16) * 0.7,
+                    height: (MediaQuery.of(context).size.width - 16) *
+                            0.7 /
+                            16 *
+                            9 +
+                        40,
+                    child: VideoPreviewWidget(
+                      title: videos[index].title ?? '',
+                      tags: videos[index].tags ?? [],
+                      timeLength: videos[index].timeLength ?? 0,
+                      coverHorizontal: videos[index].coverHorizontal ?? '',
+                      coverVertical: videos[index].coverVertical ?? '',
+                      videoViewTimes: videos[index].videoViewTimes ?? 0,
+                      detail: videos[index],
+                      isEmbeddedAds: block.isEmbeddedAds ?? false,
+                    ),
+                  ),
+                ),
+              ]),
             ),
           ),
-          VideoBlockFooter(block: block)
-        ]),
+          VideoBlockFooter(
+            block: block,
+          )
+        ],
       ),
     );
   }
