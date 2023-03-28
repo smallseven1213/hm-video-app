a1387e20efdd61b7ce4836aa5502e127a9144ed2
Cyril <smallseven@gmail.com>
2023-03-28 16:25:11 +0800

    金鋼區使用SliverAglinGrid
    金鋼區使用SliverAglinGrid

--- apps/app_gp/lib/screens/main_screen/channel/jingang.dart
+++ apps/app_gp/lib/screens/main_screen/channel/jingang.dart
@@ -134,43 +134,34 @@ class JingangList extends StatelessWidget {
           jingang.jingangDetail!.isEmpty) {
         return const SliverToBoxAdapter(child: SizedBox());
       }
+      // return SliverAlignedGrid.count(
+      //   crossAxisCount: 4,
+      //   itemCount: jingang.jingangDetail?.length ?? 0,
+      //   itemBuilder: (BuildContext context, int index) => JingangButton(
+      //     item: jingang.jingangDetail![index],
+      //     outerFrame: jingang.outerFrame ?? OuterFrame.border.value,
+      //     outerFrameStyle:
+      //         jingang.outerFrameStyle ?? OuterFrameStyle.circle.index,
+      //   ),
+      //   mainAxisSpacing: 12.0,
+      //   crossAxisSpacing: 10.0,
+      // );
+      // wrap with SliverPadding and vertical 8
       return SliverPadding(
-        padding: const EdgeInsets.symmetric(vertical: 8),
-        sliver: SliverGrid(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 4,
-            crossAxisSpacing: 30,
-            childAspectRatio: 0.95,
+        padding: const EdgeInsets.symmetric(vertical: 20.0),
+        sliver: SliverAlignedGrid.count(
+          crossAxisCount: 4,
+          itemCount: jingang.jingangDetail?.length ?? 0,
+          itemBuilder: (BuildContext context, int index) => JingangButton(
+            item: jingang.jingangDetail![index],
+            outerFrame: jingang.outerFrame ?? OuterFrame.border.value,
+            outerFrameStyle:
+                jingang.outerFrameStyle ?? OuterFrameStyle.circle.index,
           ),
-          delegate: SliverChildBuilderDelegate(
-            (BuildContext context, int index) {
-              return JingangButton(
-                item: jingang.jingangDetail![index],
-                outerFrame: jingang.outerFrame ?? OuterFrame.border.value,
-                outerFrameStyle:
-                    jingang.outerFrameStyle ?? OuterFrameStyle.circle.index,
-              );
-            },
-            childCount: jingang.jingangDetail!.length,
-          ),
+          mainAxisSpacing: 12.0,
+          crossAxisSpacing: 10.0,
         ),
       );
-      // return SliverToBoxAdapter(
-      //   child: AlignedGridView.count(
-      //     physics: const NeverScrollableScrollPhysics(),
-      //     padding: const EdgeInsets.all(8.0),
-      //     crossAxisCount: 2,
-      //     itemCount: jingang.jingangDetail?.length,
-      //     itemBuilder: (BuildContext context, int index) => JingangButton(
-      //       item: jingang.jingangDetail![index],
-      //       outerFrame: jingang.outerFrame ?? OuterFrame.border.value,
-      //       outerFrameStyle:
-      //           jingang.outerFrameStyle ?? OuterFrameStyle.circle.index,
-      //     ),
-      //     mainAxisSpacing: 12.0,
-      //     crossAxisSpacing: 10.0,
-      //   ),
-      // );
     });
   }
 }
