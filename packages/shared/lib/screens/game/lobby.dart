214058914f048b03771714d235ad3d9cb6e2b37b
Chih.lin <forever040107@yahoo.com.tw>
2023-04-26 11:09:38 +0800

    fix Scaffold

--- packages/shared/lib/screens/game/lobby.dart
+++ packages/shared/lib/screens/game/lobby.dart
@@ -10,22 +10,20 @@ class GameLobby extends StatefulWidget {
 class _GameLobbyState extends State<GameLobby> {
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async => false,
-      child: Scaffold(
-          appBar: AppBar(
-            title: const Text('游戏大厅'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('游戏大厅'),
+      ),
+      body: const Center(
+        child: Text(
+          '游戏大厅',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+            color: Colors.white, // TODO: use dark/light
           ),
-          body: const Center(
-            child: Text(
-              '游戏大厅',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
-                color: Colors.white, // TODO: use dark/light
-              ),
-            ),
-          )),
+        ),
+      ),
     );
   }
 }
