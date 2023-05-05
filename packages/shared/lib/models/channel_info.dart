ecf4bddcad5d50db50757cb1c664e2c1eda3a549
martina <iowoiy.yo@gmail.com>
2023-05-05 15:35:59 +0800

    remove: bannerImage

--- packages/shared/lib/models/channel_info.dart
+++ packages/shared/lib/models/channel_info.dart
@@ -41,31 +41,6 @@ class ChannelInfo {
   }
 }
 
-class BannerImage {
-  int? id;
-  String? photoSid;
-  String? url;
-  bool? isAutoClose;
-
-  BannerImage({id, photoSid, url, isAutoClose});
-
-  BannerImage.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    photoSid = json['photoSid'];
-    url = json['url'];
-    isAutoClose = json['isAutoClose'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['id'] = id;
-    data['photoSid'] = photoSid;
-    data['url'] = url;
-    data['isAutoClose'] = isAutoClose;
-    return data;
-  }
-}
-
 class Jingang {
   List<JingangDetail>? jingangDetail;
   bool? outerFrame;
