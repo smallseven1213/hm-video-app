b2339f79297f59f5ed909e7051d2b8dd405d4100
Cyril <smallseven@gmail.com>
2023-05-16 10:39:41 +0800

    修改篩選的UIUX

--- apps/app_gs/lib/widgets/sliver_vod_grid.dart
+++ apps/app_gs/lib/widgets/sliver_vod_grid.dart
@@ -24,69 +24,78 @@ class SliverVodGrid extends StatelessWidget {
   Widget build(BuildContext context) {
     return Obx(() {
       int totalRows = (videos.length / 2).ceil();
+      logger.i('totalRows $totalRows');
+
       return CustomScrollView(
         controller: scrollController,
         slivers: [
           ...?headerExtends,
-          SliverPadding(
-            padding: const EdgeInsets.all(8.0),
-            sliver: SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-                  int firstVideoIndex = index * 2;
-                  int secondVideoIndex = firstVideoIndex + 1;
+          if (totalRows == 0)
+            const SliverToBoxAdapter(
+              child: NoDataWidget(),
+            ),
+          if (totalRows > 0)
+            SliverPadding(
+              padding: const EdgeInsets.all(8.0),
+              sliver: SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    int firstVideoIndex = index * 2;
+                    int secondVideoIndex = firstVideoIndex + 1;
 
-                  var firstVideo = videos[firstVideoIndex];
-                  var secondVideo = secondVideoIndex < videos.length
-                      ? videos[secondVideoIndex]
-                      : null;
+                    var firstVideo = videos[firstVideoIndex];
+                    var secondVideo = secondVideoIndex < videos.length
+                        ? videos[secondVideoIndex]
+                        : null;
 
-                  // logger.i('RENDER SLIVER VOD GRID');
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: VideoPreviewWidget(
-                              id: firstVideo.id,
-                              coverVertical: firstVideo.coverVertical!,
-                              coverHorizontal: firstVideo.coverHorizontal!,
-                              timeLength: firstVideo.timeLength!,
-                              tags: firstVideo.tags!,
-                              title: firstVideo.title,
-                              videoViewTimes: firstVideo.videoViewTimes!,
+                    // logger.i('RENDER SLIVER VOD GRID');
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: VideoPreviewWidget(
+                                id: firstVideo.id,
+                                coverVertical: firstVideo.coverVertical!,
+                                coverHorizontal: firstVideo.coverHorizontal!,
+                                timeLength: firstVideo.timeLength!,
+                                tags: firstVideo.tags!,
+                                title: firstVideo.title,
+                                videoViewTimes: firstVideo.videoViewTimes!,
+                              ),
                             ),
-                          ),
-                          const SizedBox(width: 8),
-                          Expanded(
-                              child: secondVideo != null
-                                  ? VideoPreviewWidget(
-                                      id: secondVideo.id,
-                                      coverVertical: secondVideo.coverVertical!,
-                                      coverHorizontal:
-                                          secondVideo.coverHorizontal!,
-                                      timeLength: secondVideo.timeLength!,
-                                      tags: secondVideo.tags!,
-                                      title: secondVideo.title,
-                                      videoViewTimes:
-                                          secondVideo.videoViewTimes!,
-                                    )
-                                  : const SizedBox.shrink()),
-                        ],
-                      ),
-                      const SizedBox(height: 8),
-                    ],
-                  );
-                },
-                childCount: totalRows,
+                            const SizedBox(width: 8),
+                            Expanded(
+                                child: secondVideo != null
+                                    ? VideoPreviewWidget(
+                                        id: secondVideo.id,
+                                        coverVertical:
+                                            secondVideo.coverVertical!,
+                                        coverHorizontal:
+                                            secondVideo.coverHorizontal!,
+                                        timeLength: secondVideo.timeLength!,
+                                        tags: secondVideo.tags!,
+                                        title: secondVideo.title,
+                                        videoViewTimes:
+                                            secondVideo.videoViewTimes!,
+                                      )
+                                    : const SizedBox.shrink()),
+                          ],
+                        ),
+                        const SizedBox(height: 8),
+                      ],
+                    );
+                  },
+                  childCount: totalRows,
+                ),
               ),
             ),
-          ),
-          if (hasMoreData) const SliverVideoPreviewSkeletonList(),
-          if (!hasMoreData && noMoreWidget != null)
+          if (hasMoreData && totalRows > 0)
+            const SliverVideoPreviewSkeletonList(),
+          if (!hasMoreData && totalRows > 0)
             SliverToBoxAdapter(
               child: noMoreWidget,
             ),
