e9246ed193773ea819684987b0179db522fa3e68
martina <iowoiy.yo@gmail.com>
2023-05-24 13:42:59 +0800

    fix: 首頁_單模板頻道，在template = 3(二橫圖小封面）時，廣告未處理

--- apps/app_gs/lib/widgets/base_video_block_templates/template_3.dart
+++ apps/app_gs/lib/widgets/base_video_block_templates/template_3.dart
@@ -1,10 +1,34 @@
-// class Template3 is a stateless widget, only has props: List<Vod> vods
-
 import 'package:flutter/material.dart';
+import 'package:shared/models/banner_photo.dart';
 import 'package:shared/models/block_image_ratio.dart';
 import 'package:shared/models/vod.dart';
 
+import '../channel_area_banner.dart';
 import '../video_block_grid_view_row.dart';
+
+List<List<Vod>> organizeRowData(List videos) {
+  List<List<Vod>> resultArray = [];
+  List<Vod> tempArray = [];
+
+  for (int i = 0; i < videos.length; i++) {
+    tempArray.add(videos[i]);
+    bool hasAreaAd = videos[i].dataType == VideoType.areaAd.index;
+
+    if (hasAreaAd) {
+      resultArray.add(tempArray);
+      tempArray = [];
+    } else if (tempArray.length == 2) {
+      resultArray.add(tempArray);
+      tempArray = [];
+    }
+  }
+
+  if (tempArray.isNotEmpty) {
+    resultArray.add(tempArray);
+  }
+
+  return resultArray;
+}
 
 SliverChildBuilderDelegate baseVideoBlockTemplate3({
   required List<Vod> vods,
@@ -12,18 +36,27 @@ SliverChildBuilderDelegate baseVideoBlockTemplate3({
   return SliverChildBuilderDelegate(
     (BuildContext context, int index) {
       // 每2個vods組成一個videos
-      var videos = vods.sublist(index * 2, index * 2 + 2);
+      List<List<Vod>> result = organizeRowData(vods);
       return Padding(
         // padding bottom 8
         padding: const EdgeInsets.only(bottom: 8.0),
-        child: VideoBlockGridViewRow(
-          videoData: videos,
-          gridLength: 2,
-          imageRatio: BlockImageRatio.block3.ratio,
-          isEmbeddedAds: true,
-        ),
+        child: result[index][0].dataType == VideoType.areaAd.index
+            ? ChannelAreaBanner(
+                image: BannerPhoto.fromJson({
+                  'id': result[index][0].id ?? 0,
+                  'url': result[index][0].adUrl ?? '',
+                  'photoSid': result[index][0].coverHorizontal ?? '',
+                  'isAutoClose': false,
+                }),
+              )
+            : VideoBlockGridViewRow(
+                videoData: result[index],
+                gridLength: 2,
+                imageRatio: BlockImageRatio.block3.ratio,
+                isEmbeddedAds: true,
+              ),
       );
     },
-    childCount: vods.length ~/ 2,
+    childCount: organizeRowData(vods).length,
   );
 }
