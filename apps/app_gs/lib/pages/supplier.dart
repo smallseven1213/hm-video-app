5880246961f68945eb5d705e6e954a6c30b2d60e
Cyril <smallseven@gmail.com>
2023-05-31 16:58:22 +0800

    修正供應商沒有loadmore的問題
    修正供應商沒有loadmore的問題

--- apps/app_gs/lib/pages/supplier.dart
+++ apps/app_gs/lib/pages/supplier.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shared/controllers/supplier_vod_controller.dart';
 import 'package:shared/widgets/float_page_back_button.dart';
 
 import '../screens/supplier/card.dart';
 import '../screens/supplier/list.dart';
+import '../widgets/list_no_more.dart';
+import '../widgets/sliver_video_preview_skelton_list.dart';
 
 class SupplierPage extends StatelessWidget {
   final int id;
@@ -14,23 +18,34 @@ class SupplierPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final ScrollController parentScrollController = ScrollController();
-    return Stack(
-      children: [
-        CustomScrollView(
-          physics: const BouncingScrollPhysics(),
-          slivers: [
-            SliverToBoxAdapter(
-              child: SizedBox(
-                height: MediaQuery.of(context).padding.top,
-              ),
-            ),
-            SupplierCard(id: id),
-            SupplierVods(id: id, scrollController: parentScrollController)
-          ],
-        ),
-        const FloatPageBackButton()
-      ],
+    final ScrollController scrollController = ScrollController();
+    final vodController = SupplierVodController(
+        supplierId: id, scrollController: scrollController);
+    return Scaffold(
+      body: Stack(
+        children: [
+          Obx(() => CustomScrollView(
+                controller: vodController.scrollController,
+                physics: const BouncingScrollPhysics(),
+                slivers: [
+                  SliverToBoxAdapter(
+                    child: SizedBox(
+                      height: MediaQuery.of(context).padding.top,
+                    ),
+                  ),
+                  SupplierCard(id: id),
+                  SupplierVods(id: id, vodList: vodController.vodList.value),
+                  if (vodController.hasMoreData.value)
+                    const SliverVideoPreviewSkeletonList(),
+                  if (!vodController.hasMoreData.value)
+                    const SliverToBoxAdapter(
+                      child: ListNoMore(),
+                    )
+                ],
+              )),
+          const FloatPageBackButton()
+        ],
+      ),
     );
   }
 }
--- apps/app_gs/lib/screens/supplier/list.dart
+++ apps/app_gs/lib/screens/supplier/list.dart
@@ -3,57 +3,53 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared/controllers/supplier_vod_controller.dart';
 import 'package:shared/enums/app_routes.dart';
+import 'package:shared/models/vod.dart';
 import 'package:shared/navigator/delegate.dart';
 
 const gridRatio = 128 / 227;
 
 class SupplierVods extends StatelessWidget {
   final int id;
-  final ScrollController scrollController;
-  const SupplierVods(
-      {Key? key, required this.id, required this.scrollController})
+  final List<Vod> vodList;
+  const SupplierVods({Key? key, required this.id, required this.vodList})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final vodController = SupplierVodController(
-        supplierId: id, scrollController: scrollController);
-    return Obx(() {
-      return SliverGrid(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          childAspectRatio: gridRatio,
-          crossAxisSpacing: 1,
-          mainAxisSpacing: 1,
-        ),
-        delegate: SliverChildBuilderDelegate(
-          (BuildContext context, int index) {
-            var vod = vodController.vodList.value[index];
-            return VideoPreviewWidget(
-                id: id,
-                onOverrideRedirectTap: () {
-                  MyRouteDelegate.of(context).push(
-                    AppRoutes.shortsBySupplier.value,
-                    args: {'videoId': vod.id, 'supplierId': id},
-                    removeSamePath: true,
-                  );
-                },
-                film: 2,
-                hasRadius: false,
-                hasTitle: false,
-                imageRatio: gridRatio,
-                displayCoverVertical: true,
-                coverVertical: vod.coverVertical!,
-                coverHorizontal: vod.coverHorizontal!,
-                timeLength: vod.timeLength!,
-                tags: vod.tags!,
-                title: vod.title,
-                videoViewTimes: vod.videoViewTimes!,
-                videoCollectTimes: vod.videoCollectTimes!);
-          },
-          childCount: vodController.vodList.value.length,
-        ),
-      );
-    });
+    return SliverGrid(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        childAspectRatio: gridRatio,
+        crossAxisSpacing: 1,
+        mainAxisSpacing: 1,
+      ),
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          var vod = vodList[index];
+          return VideoPreviewWidget(
+              id: id,
+              onOverrideRedirectTap: () {
+                MyRouteDelegate.of(context).push(
+                  AppRoutes.shortsBySupplier.value,
+                  args: {'videoId': vod.id, 'supplierId': id},
+                  removeSamePath: true,
+                );
+              },
+              film: 2,
+              hasRadius: false,
+              hasTitle: false,
+              imageRatio: gridRatio,
+              displayCoverVertical: true,
+              coverVertical: vod.coverVertical!,
+              coverHorizontal: vod.coverHorizontal!,
+              timeLength: vod.timeLength!,
+              tags: vod.tags!,
+              title: vod.title,
+              videoViewTimes: vod.videoViewTimes!,
+              videoCollectTimes: vod.videoCollectTimes!);
+        },
+        childCount: vodList.length,
+      ),
+    );
   }
 }
