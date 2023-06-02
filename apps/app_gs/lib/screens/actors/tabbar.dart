30f22733eb1abba4cb8895d4838bba8ca7c7e1a0
martina <iowoiy.yo@gmail.com>
2023-06-02 14:15:59 +0800

    ui 咻咻

--- apps/app_gs/lib/screens/actors/tabbar.dart
+++ apps/app_gs/lib/screens/actors/tabbar.dart
@@ -47,6 +47,7 @@ class _ActorsTabBarState extends State<ActorsTabBar>
               .where((e) => e.name != null)
               .map((e) => e.name!)
               .toList(),
+          padding: const EdgeInsets.all(0),
         ));
   }
 }
