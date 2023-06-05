5110f239f26bf0ba7e820b18f6b130fad642dec3
Chih.lin <forever040107@yahoo.com.tw>
2023-06-05 13:38:57 +0800

    測試從遊戲返回大廳

--- packages/game/lib/screens/game_webview_screen/index.dart
+++ packages/game/lib/screens/game_webview_screen/index.dart
@@ -1,22 +1,12 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:game/controllers/game_config_controller.dart';
 import 'package:game/models/game_list.dart';
 import 'package:game/screens/game_webview_screen/game_webview_toggle_button.dart';
 import 'package:game/widgets/draggable_button.dart';
 import 'package:logger/logger.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:get/get.dart';
 import 'package:pointer_interceptor/pointer_interceptor.dart';
 
-import 'package:shared/navigator/delegate.dart';
-
-import 'package:game/services/game_system_config.dart';
-import 'package:game/utils/showConfirmDialog.dart';
 import 'package:game/widgets/h5webview.dart';
-
-import '../../enums/game_app_routes.dart';
 
 final logger = Logger();
 
@@ -29,164 +19,6 @@ class GameLobbyWebview extends StatefulWidget {
 
   @override
   State<GameLobbyWebview> createState() => _GameLobbyWebview();
-}
-
-class ButtonWidget extends StatefulWidget {
-  final Function toggleButtonRow;
-  const ButtonWidget({
-    Key? key,
-    required this.toggleButtonRow,
-  }) : super(key: key);
-
-  @override
-  State<ButtonWidget> createState() => _ButtonWidget();
-}
-
-// 建立一個ButtonWidget，用來放兩個floatingActionButton
-class _ButtonWidget extends State<ButtonWidget> {
-  final systemConfig = GameSystemConfig();
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final orientation = MediaQuery.of(context).orientation;
-    final gameConfigController = Get.put(GameConfigController());
-
-    return PointerInterceptor(
-      child: RotatedBox(
-        quarterTurns: orientation == Orientation.portrait ? 1 : 0,
-        child: Container(
-          height: 88,
-          width: 288,
-          decoration: BoxDecoration(
-            color: Colors.black.withOpacity(0.5),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              InkWell(
-                onTap: () {
-                  logger.i('返回大廳');
-                  showConfirmDialog(
-                    context: context,
-                    title: '退出遊戲',
-                    content: '你真的要退出遊戲嗎？',
-                    rotate: orientation == Orientation.portrait ? true : false,
-                    onConfirm: () {
-                      Navigator.of(context).pop();
-                      MyRouteDelegate.of(context).popRoute();
-                    },
-                    onCancel: () {
-                      MyRouteDelegate.of(context).popRoute();
-                    },
-                  );
-                },
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SvgPicture.asset(
-                      'packages/game/assets/svg/icon-game-home.svg',
-                      width: 32,
-                      height: 32,
-                    ),
-                    const SizedBox(height: 5),
-                    const Text(
-                      "返回大廳",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 12,
-                        fontFeatures: [FontFeature.proportionalFigures()],
-                      ),
-                    ), // <-- Text
-                  ],
-                ),
-              ),
-              InkWell(
-                onTap: () {
-                  launch(
-                      '${systemConfig.apiHost}/public/domains/domain/customer-services');
-                },
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SvgPicture.asset(
-                      'packages/game/assets/svg/icon-game-service.svg',
-                      width: 32,
-                      height: 32,
-                    ),
-                    const SizedBox(height: 5),
-                    const Text(
-                      "客服",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 12,
-                      ),
-                    ), // <-- Text
-                  ],
-                ),
-              ),
-              InkWell(
-                onTap: () {
-                  logger.i('充值');
-                  Navigator.of(context).pop();
-                  MyRouteDelegate.of(context).push(
-                    gameConfigController.switchPaymentPage.value ==
-                            switchPaymentPageType['list']
-                        ? GameAppRoutes.depositList.value
-                        : GameAppRoutes.depositPolling.value,
-                  );
-                },
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SvgPicture.asset(
-                      'packages/game/assets/svg/icon-game-deposit.svg',
-                      width: 32,
-                      height: 32,
-                    ),
-                    const SizedBox(height: 5),
-                    const Text(
-                      "充值",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 12,
-                      ),
-                    ), // <-- Text
-                  ],
-                ),
-              ),
-              InkWell(
-                onTap: () => widget.toggleButtonRow(),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SvgPicture.asset(
-                      'packages/game/assets/svg/icon-game-close.svg',
-                      width: 32,
-                      height: 32,
-                    ),
-                    const SizedBox(height: 5),
-                    const Text(
-                      "關閉",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 12,
-                      ),
-                    ), // <-- Text
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
 }
 
 class _GameLobbyWebview extends State<GameLobbyWebview> {
