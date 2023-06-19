b2c2d5616b88ec668aace0752a4668ce4f5faaf7
Cyril <smallseven@gmail.com>
2023-06-19 09:40:58 +0800

    修改開發中問題

--- apps/app_gs/lib/main.dart
+++ apps/app_gs/lib/main.dart
@@ -16,9 +16,8 @@ void main() async {
   usePathUrlStrategy();
 
   await SentryFlutter.init((options) {
-    options.dsn = kDebugMode
-        ? null
-        : 'https://1b4441d1f4464b93a69208281ab77d4b@sentry.hmtech.site/2';
+    options.dsn =
+        'https://1b4441d1f4464b93a69208281ab77d4b@sentry.hmtech.site/2';
     // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
     // We recommend adjusting this value in production.
     options.tracesSampleRate = kDebugMode ? 0 : 1.0;
