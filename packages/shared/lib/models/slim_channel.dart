3d0f2cd2c7b1d113a583c8df7f8c6ad86210d7f1
Cyril <smallseven@gmail.com>
2023-06-20 15:22:04 +0800

    修修
    - 首頁測試按鈕移除
    - channel api list異動
    - video player area web版測試

--- packages/shared/lib/models/slim_channel.dart
+++ packages/shared/lib/models/slim_channel.dart
@@ -3,14 +3,16 @@ class SlimChannel {
   final int id;
   final String name;
   final int style;
+  final bool isSearch;
 
-  SlimChannel(this.id, this.name, this.style);
+  SlimChannel(this.id, this.name, this.style, this.isSearch);
 
   factory SlimChannel.fromJson(Map<String, dynamic> json) {
     return SlimChannel(
       json['id'],
       json['name'],
       json['style'],
+      json['isSearch'],
     );
   }
 }
