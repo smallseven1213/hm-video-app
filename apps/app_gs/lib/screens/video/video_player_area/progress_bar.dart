9eb1d3dbf08603903a1bea47b40dd7ed49d8afc2
Cyril <smallseven@gmail.com>
2023-06-27 14:28:24 +0800

    修修
    - 修影片拖拉bar
    - 加回flutter

--- apps/app_gs/lib/screens/video/video_player_area/progress_bar.dart
+++ apps/app_gs/lib/screens/video/video_player_area/progress_bar.dart
@@ -1,42 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 import 'package:video_player/video_player.dart';
 
-class ProgressBar extends StatelessWidget {
+import 'new_progress_bar.dart';
+
+class ProgressBar extends StatefulWidget {
   final VideoPlayerController controller;
   final Function toggleFullscreen;
   final bool isFullscreen;
   final double opacity;
+  final Function onDragUpdate;
   const ProgressBar({
-    super.key,
+    Key? key,
     required this.controller,
     required this.toggleFullscreen,
     required this.isFullscreen,
     required this.opacity,
-  });
+    required this.onDragUpdate,
+  }) : super(key: key);
+
+  @override
+  _ProgressBarState createState() => _ProgressBarState();
+}
+
+final logger = Logger();
+
+class _ProgressBarState extends State<ProgressBar> {
+  late int position = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.controller.addListener(() {
+      if (mounted) {
+        var pos = widget.controller.value.position.toString();
+        List<String> parts = pos.split('.')[0].split(':');
+
+        int hours = int.parse(parts[0]);
+        int minutes = int.parse(parts[1]);
+        int seconds = int.parse(parts[2]);
+
+        Duration duration =
+            Duration(hours: hours, minutes: minutes, seconds: seconds);
+        int totalSeconds = duration.inSeconds;
+        setState(() {
+          position = totalSeconds;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    widget.controller.removeListener(() {});
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Positioned(
-      bottom: isFullscreen ? 30 : 0,
+      bottom: widget.isFullscreen ? 30 : 0,
       left: 0,
       right: 0,
       child: AnimatedOpacity(
-        opacity: opacity,
+        opacity: widget.opacity,
         duration: const Duration(milliseconds: 300),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             ValueListenableBuilder(
-              valueListenable: controller,
+              valueListenable: widget.controller,
               builder: (context, value, child) {
                 return IconButton(
                   onPressed: () {
-                    controller.value.isPlaying
-                        ? controller.pause()
-                        : controller.play();
+                    widget.controller.value.isPlaying
+                        ? widget.controller.pause()
+                        : widget.controller.play();
                   },
                   icon: Icon(
-                    controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+                    widget.controller.value.isPlaying
+                        ? Icons.pause
+                        : Icons.play_arrow,
                     color: Colors.white,
                     size: 18,
                   ),
@@ -44,31 +88,32 @@ class ProgressBar extends StatelessWidget {
               },
             ),
             Text(
-              controller.value.position.toString().split('.')[0],
+              widget.controller.value.position.toString().split('.')[0],
               style: const TextStyle(color: Colors.white),
             ),
             const SizedBox(width: 5.0),
             Expanded(
-              child: VideoProgressIndicator(
-                controller,
-                allowScrubbing: true,
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                colors: VideoProgressColors(
-                  playedColor: Colors.blue,
-                  bufferedColor: Colors.grey,
-                  backgroundColor: Colors.white.withOpacity(0.3),
-                ),
+              child: NewProgressBar(
+                test1: position.toString(),
+                currentProgress: position.toDouble(),
+                videoDuration:
+                    widget.controller.value.duration.inSeconds.toDouble(),
+                onDragUpdate: (newPositionInSeconds) {
+                  widget.controller
+                      .seekTo(Duration(seconds: newPositionInSeconds.toInt()));
+                  widget.onDragUpdate();
+                },
               ),
             ),
             const SizedBox(width: 5.0),
             Text(
-              controller.value.duration.toString().split('.')[0],
+              widget.controller.value.duration.toString().split('.')[0],
               style: const TextStyle(color: Colors.white),
             ),
             IconButton(
-              onPressed: () => toggleFullscreen(),
+              onPressed: () => widget.toggleFullscreen(),
               icon: Icon(
-                  isFullscreen
+                  widget.isFullscreen
                       ? Icons.close_fullscreen_rounded
                       : Icons.fullscreen,
                   color: Colors.white),
