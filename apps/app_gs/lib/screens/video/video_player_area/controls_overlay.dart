1181809b31dbe9a17226c372ac1fd662871ca19f
Cyril <smallseven@gmail.com>
2023-06-27 16:45:22 +0800

    目前進度

--- apps/app_gs/lib/screens/video/video_player_area/controls_overlay.dart
+++ apps/app_gs/lib/screens/video/video_player_area/controls_overlay.dart
@@ -15,45 +15,76 @@ class ControlsOverlay extends StatefulWidget {
 
 class ControlsOverlayState extends State<ControlsOverlay> {
   late ObservableVideoPlayerController ovpController;
+  int videoDuration = 0; // 影片總長度
+  int videoPosition = 0; // 影片目前進度
 
   @override
   void initState() {
     ovpController =
         Get.find<ObservableVideoPlayerController>(tag: widget.videoUrl);
+
+    ovpController.videoPlayerController.addListener(() {
+      if (mounted) {
+        setState(() {
+          videoDuration = ovpController
+              .videoPlayerController.value.duration.inSeconds
+              .toInt();
+          videoPosition = ovpController
+              .videoPlayerController.value.position.inSeconds
+              .toInt();
+        });
+      }
+    });
     super.initState();
+  }
+
+  // 主動更新影片進度
+  void updateVideoPosition(Duration newPosition) {
+    ovpController.videoPlayerController.seekTo(newPosition);
   }
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
       var boxWidth = constraints.maxWidth;
-      return Obx(() {
-        // 影片總時長
-        final double videoDurationInSeconds = ovpController
-            .videoPlayerController.value.duration.inSeconds
-            .toDouble();
-        // 目前播到的位置
-        final double videoPositionInSeconds = ovpController
-            .videoPlayerController.value.position.inSeconds
-            .toDouble();
-        return GestureDetector(
-          child: Stack(
-            children: <Widget>[
-              Positioned(
-                  child: Container(
-                color: Colors.white,
-                child: Column(
-                  children: [
-                    Text('boxWidth: $boxWidth'),
-                    Text('videoDurationInSeconds: $videoDurationInSeconds'),
-                    Text('videoPositionInSeconds: $videoPositionInSeconds'),
-                  ],
-                ),
-              ))
-            ],
-          ),
-        );
-      });
+      return GestureDetector(
+        onHorizontalDragUpdate: (details) {
+          int newPositionSeconds = videoPosition + details.delta.dx.toInt();
+          // Make sure we are within the video duration
+          if (newPositionSeconds < 0) newPositionSeconds = 0;
+          if (newPositionSeconds > videoDuration) {
+            newPositionSeconds = videoDuration;
+          }
+
+          updateVideoPosition(Duration(seconds: newPositionSeconds));
+        },
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+                child: Container(
+              color: Colors.white,
+              child: Column(
+                children: [
+                  Text('boxWidth: $boxWidth'),
+                  Text('videoDurationInSeconds: $videoDuration'),
+                  Text('videoPositionInSeconds: $videoPosition'),
+                ],
+              ),
+            )),
+            Positioned(
+              bottom: 0,
+              child: Slider(
+                value: videoPosition.toDouble(),
+                min: 0,
+                max: videoDuration.toDouble(),
+                onChanged: (double value) {
+                  updateVideoPosition(Duration(seconds: value.toInt()));
+                },
+              ),
+            )
+          ],
+        ),
+      );
     });
   }
 }
