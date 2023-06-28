d487004c57428dd5cd6199df026913df555acac6
martina <iowoiy.yo@gmail.com>
2023-06-28 15:27:28 +0800

    試試進度條效果

--- apps/app_gs/lib/widgets/shortcard/index.dart
+++ apps/app_gs/lib/widgets/shortcard/index.dart
@@ -8,7 +8,6 @@ import 'package:shared/controllers/video_player_controller.dart';
 import 'package:shared/models/vod.dart';
 import 'package:shared/utils/controller_tag_genarator.dart';
 import 'package:shared/widgets/video_player/player.dart';
-import 'package:shared/widgets/video_player/progress.dart';
 import 'package:video_player/video_player.dart';
 import '../short_bottom_area.dart';
 import '../wave_loading.dart';
@@ -45,6 +44,8 @@ class ShortCardState extends State<ShortCard> {
   late ObservableVideoPlayerController obsVideoPlayerController;
   double trackHeight = 2.0;
   double startHorizontalDragX = 0.0;
+
+  bool isDragging = false;
 
   @override
   void initState() {
@@ -88,6 +89,7 @@ class ShortCardState extends State<ShortCard> {
   @override
   Widget build(BuildContext context) {
     final screen = MediaQuery.of(context);
+    var screenWidth = MediaQuery.of(context).size.width;
 
     return Obx(() {
       var isLoading = videoDetailController.isLoading.value;
@@ -143,20 +145,53 @@ class ShortCardState extends State<ShortCard> {
               bottom: 52 + screen.padding.bottom,
               left: -24,
               right: -24,
-              child: ValueListenableBuilder<VideoPlayerValue>(
-                valueListenable: obsVideoPlayerController.videoPlayerController,
-                builder: (context, value, _) => VideoProgressSlider(
-                  controller: obsVideoPlayerController.videoPlayerController,
-                  position: value.position,
-                  duration: value.duration,
-                  swatch: const Color(0xffFFC700),
-                  trackHeight: trackHeight,
-                  onChangeStart: (value) => setState(() {
-                    trackHeight = 4;
-                  }),
-                  onChangeEnd: (value) => setState(() {
-                    trackHeight = 2;
-                  }),
+              child: GestureDetector(
+                onHorizontalDragUpdate: (details) {
+                  final box = context.findRenderObject()! as RenderBox;
+                  final deltaX =
+                      startHorizontalDragX - details.globalPosition.dx;
+                  final percentageDelta = deltaX / box.size.width;
+                  final videoDuration = obsVideoPlayerController
+                      .videoPlayerController.value.duration.inSeconds;
+                  final newPositionSeconds = obsVideoPlayerController
+                          .videoPlayerController.value.position.inSeconds -
+                      (videoDuration * percentageDelta);
+
+                  // 拖動影片進度
+                  if (newPositionSeconds >= 0 &&
+                      newPositionSeconds <= videoDuration) {
+                    final newPosition =
+                        Duration(seconds: newPositionSeconds.round());
+                    obsVideoPlayerController.videoPlayerController
+                        .seekTo(newPosition);
+                  }
+                  startHorizontalDragX = details.globalPosition.dx;
+                },
+                onHorizontalDragStart: (details) {
+                  startHorizontalDragX = details.globalPosition.dx;
+                  setState(() {
+                    isDragging = true;
+                  });
+                },
+                onHorizontalDragEnd: (details) {
+                  setState(() {
+                    isDragging = false;
+                  });
+                },
+                child: AnimatedContainer(
+                  color: Colors.white.withOpacity(0),
+                  duration: const Duration(milliseconds: 300),
+                  height: isDragging ? 40 : 35,
+                  child: VideoProgressIndicator(
+                    obsVideoPlayerController.videoPlayerController,
+                    allowScrubbing: false,
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    colors: VideoProgressColors(
+                      playedColor: const Color(0xffFFC700),
+                      bufferedColor: Colors.grey,
+                      backgroundColor: Colors.white.withOpacity(0.3),
+                    ),
+                  ),
                 ),
               ),
             ),
