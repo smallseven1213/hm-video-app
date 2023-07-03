454c48829c30993a6c0be5e1b100790a4ce07f5b
Chih.lin <forever040107@yahoo.com.tw>
2023-07-03 15:09:33 +0800

    test GameStartupController

--- packages/game/lib/widgets/game_startup.dart
+++ packages/game/lib/widgets/game_startup.dart
@@ -1,16 +1,13 @@
-/// 如果任何App要使用遊戲相關功能，需要在RootWidget中加入GetX所做成的GameStartup Controller
-/// 在初始化(Get.put) GameStartupController時，就要先設定下面一些東西:
-/// 1. backToAppHome: () {}, 這裡在遊戲要"返回App端時", 就會call Controller.backToAppHome()然後返回app
-
 import 'package:get/get.dart';
 import 'package:shared/enums/app_routes.dart';
 import 'package:shared/navigator/delegate.dart';
 
 class GameStartupController extends GetxController {
   void goBackToAppHome(context) {
-    MyRouteDelegate.of(context).pushAndRemoveUntil(
+    MyRouteDelegate.of(context).push(
       AppRoutes.home.value,
       hasTransition: false,
+      removeSamePath: true,
       args: {'defaultScreenKey': '/game'},
     );
   }
