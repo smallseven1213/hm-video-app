91603c65a2dee3987fd063000b355b4c2f78619d
martina <iowoiy.yo@gmail.com>
2023-07-12 10:45:16 +0800

    改改

--- apps/app_gs/lib/screens/video/nested_tab_bar_view/index.dart
+++ apps/app_gs/lib/screens/video/nested_tab_bar_view/index.dart
@@ -65,6 +65,7 @@ class NestedTabBarViewState extends State<NestedTabBarView>
   void dispose() {
     // _parentScrollController.dispose();
     _tabController.dispose();
+
     super.dispose();
   }
 
@@ -123,21 +124,30 @@ class NestedTabBarViewState extends State<NestedTabBarView>
                 child: VideoScreenBanner(),
               ),
             ),
-            SliverToBoxAdapter(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 8, right: 8, left: 8),
-                child: GSTabBar(
-                  controller: _tabController,
-                  tabs: tabs,
-                ),
-              ),
-            ),
+            // SliverToBoxAdapter(
+            //   child: Padding(
+            //     padding: const EdgeInsets.only(top: 8, right: 8, left: 8),
+            //     child: GSTabBar(
+            //       controller: _tabController,
+            //       tabs: tabs,
+            //     ),
+            //   ),
+            // ),
+
+            SliverPersistentHeader(
+              pinned: true,
+              delegate: TabBarHeaderDelegate(_tabController, tabs),
+            )
+            // SliverPersistentHeader(
+            //   pinned: true,
+            //   delegate: TabBarHeaderDelegate(_tabController, tabs),
+            // )
           ];
         },
         body: Obx(
           () => TabBarView(
             controller: _tabController,
-            physics: const BouncingScrollPhysics(),
+            physics: const NeverScrollableScrollPhysics(),
             children: [
               if (widget.videoDetail.actors!.isNotEmpty)
                 SliverVodGrid(
