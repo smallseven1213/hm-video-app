efe0b91ca670536483d082a60def9d88e0c60848
martina <iowoiy.yo@gmail.com>
2023-07-21 13:37:00 +0800

    fix: format 加上防呆

--- packages/shared/lib/utils/sid_image_result_decode.dart
+++ packages/shared/lib/utils/sid_image_result_decode.dart
@@ -1,3 +1,7 @@
+import 'package:logger/logger.dart';
+
+final logger = Logger();
+
 String getSidImageDecode(
   String word, {
   int posStartAt = 0,
@@ -5,8 +9,13 @@ String getSidImageDecode(
   int posLen = 3,
   int passLen = 16,
 }) {
-  var dePos = word.substring(posStartAt, posLen);
-  var pos = int.parse(dePos.replaceAll(posChar, ''));
-  return word.substring(posLen, posLen + pos) +
-      word.substring(posLen + pos + passLen);
+  try {
+    var dePos = word.substring(posStartAt, posLen);
+    var pos = int.parse(dePos.replaceAll(posChar, ''));
+    return word.substring(posLen, posLen + pos) +
+        word.substring(posLen + pos + passLen);
+  } catch (e) {
+    logger.i('format error: $e');
+    return '';
+  }
 }
