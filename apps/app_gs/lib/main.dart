310448f4278c5ccc4c7c043ce522d870fb42318c
Cyril <smallseven@gmail.com>
2023-07-22 10:27:48 +0800

    dljson host from runningMain

--- apps/app_gs/lib/main.dart
+++ apps/app_gs/lib/main.dart
@@ -9,6 +9,8 @@ import 'config/colors.dart';
 import './routes/app_routes.dart' as app_routes;
 import './routes/game_routes.dart' as game_routes;
 
+const env = String.fromEnvironment('ENV', defaultValue: 'prod');
+
 void main() async {
   final allRoutes = {
     ...app_routes.appRoutes,
@@ -18,6 +20,11 @@ void main() async {
   runningMain(
       'https://1b4441d1f4464b93a69208281ab77d4b@sentry.hmtech.site/2',
       allRoutes.keys.first,
+      [
+        'https://dl.dlgs.app/$env/dl.json',
+        'https://dl.dlgs.one/$env/dl.json',
+        'https://dl.dlgs.info/$env/dl.json',
+      ],
       allRoutes,
       AppColors.colors,
       ({String? text}) => Loading(loadingText: text ?? '正在加载...'),
--- apps/app_tt/lib/main.dart
+++ apps/app_tt/lib/main.dart
@@ -5,6 +5,8 @@ import './routes/app_routes.dart'
     if (dart.library.html) './routes/app_routes_web.dart' as app_routes;
 import './routes/game_routes.dart' as game_routes;
 
+const env = String.fromEnvironment('ENV', defaultValue: 'prod');
+
 void main() async {
   final allRoutes = {
     ...app_routes.appRoutes,
@@ -14,6 +16,11 @@ void main() async {
   runningMain(
       'https://1b4441d1f4464b93a69208281ab77d4b@sentry.hmtech.site/2',
       allRoutes.keys.first,
+      [
+        'https://dl.dlgs.app/$env/dl.json',
+        'https://dl.dlgs.one/$env/dl.json',
+        'https://dl.dlgs.info/$env/dl.json',
+      ],
       allRoutes,
       AppColors.colors,
       ({String? text}) => Loading(loadingText: text ?? '正在加载...'),
