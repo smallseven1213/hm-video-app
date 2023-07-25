974eb865c4f10e11a0e764cb089fc5c2f81b4a32
Cyril <smallseven@gmail.com>
2023-07-25 14:04:31 +0800

    應用中心重構

---
+++ apps/app_tt/lib/widgets/my_app_bar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const MyAppBar({
+    Key? key,
+    this.title,
+    this.titleWidget,
+    this.backgroundColor,
+    this.bottom,
+    this.actions,
+  }) : super(key: key);
+
+  final String? title;
+  final Widget? titleWidget;
+  final Color? backgroundColor;
+  final List<Widget>? actions;
+  final PreferredSizeWidget? bottom;
+
+  @override
+  Size get preferredSize {
+    final double bottomHeight = bottom?.preferredSize.height ?? 0;
+    return Size.fromHeight(kToolbarHeight + bottomHeight);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      leading: Navigator.canPop(context)
+          ? GestureDetector(
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              child: Container(
+                height: 56.0,
+                width: 56.0,
+                color: Colors.transparent,
+                child: const Center(
+                  child: Icon(Icons.arrow_back_ios_new, size: 16),
+                ),
+              ),
+            )
+          : Container(),
+      backgroundColor: backgroundColor ?? Colors.white,
+      title: titleWidget ??
+          Text(
+            title!,
+            style: const TextStyle(
+              fontSize: 15,
+            ),
+          ),
+      bottom: bottom,
+      actions: actions, // 將actions添加到AppBar
+    );
+  }
+}
