4690f1c5197a247276c64e71bb32b25499c1664b
Cyril <smallseven@gmail.com>
2023-07-25 16:05:38 +0800

    重構LayoutTabBuilder

---
+++ apps/app_tt/lib/screens/layout_home_screen/channel_search_bar.dart
@@ -0,0 +1,76 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shared/controllers/user_search_history_controller.dart';
+import 'package:shared/controllers/video_popular_controller.dart';
+import 'package:shared/enums/app_routes.dart';
+import 'package:shared/navigator/delegate.dart';
+
+class ChannelSearchBar extends StatelessWidget {
+  ChannelSearchBar({Key? key}) : super(key: key);
+
+  final VideoPopularController videoPopularController =
+      Get.find<VideoPopularController>();
+
+  @override
+  Widget build(BuildContext context) {
+    logger.i('RENDER SearchBar');
+    return SizedBox(
+      height: 50,
+      width: double.infinity,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Obx(() {
+              int listLength = videoPopularController.data.length;
+              int randomIndex = Random().nextInt(listLength);
+              String randomTitle = videoPopularController.data.isNotEmpty
+                  ? videoPopularController.data[randomIndex].title
+                  : '';
+
+              return Container();
+
+              // return StaticSearchInput(
+              //   defaultValue: randomTitle,
+              //   onSearchButtonClick: () {
+              //     MyRouteDelegate.of(context).push(AppRoutes.search, args: {
+              //       'inputDefaultValue': randomTitle,
+              //       'autoSearch': true
+              //     });
+              //     Get.find<UserSearchHistoryController>().add(randomTitle);
+              //   },
+              //   onInputClick: () {
+              //     MyRouteDelegate.of(context).push(AppRoutes.search, args: {
+              //       'inputDefaultValue': randomTitle,
+              //       'autoSearch': false
+              //     });
+              //   },
+              // );
+            }),
+          ),
+          GestureDetector(
+            onTap: () {
+              MyRouteDelegate.of(context).push(AppRoutes.filter);
+            },
+            child: Container(
+                width: 40,
+                height: 60,
+                // color: AppColors.colors[ColorKeys.background],
+                child: const Center(
+                  child: SizedBox(
+                    width: 24,
+                    height: 24,
+                    child: Image(
+                      image: AssetImage(
+                          'assets/images/home_search_bar_filter.png'),
+                    ),
+                  ),
+                )),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ packages/shared/lib/widgets/layout_tab_builder.dart
@@ -0,0 +1,106 @@
+//  is a statefull widget, has no props, return a empty Container
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controllers/channel_screen_tab_controller.dart';
+import '../controllers/layout_controller.dart';
+import 'layout_tab_item_builder.dart';
+
+class LayoutTabBuilder extends StatefulWidget {
+  final int layoutId;
+  final LayoutTabBarItemBuilder Function({
+    required int layoutId,
+    required int index,
+    required String name,
+  }) barItemWidget;
+  final Function({
+    required TabController tabController,
+    required List<Widget> tabItems,
+    required Function(int) onTapTabItem,
+  }) tabbarWidget;
+
+  const LayoutTabBuilder({
+    Key? key,
+    required this.layoutId,
+    required this.tabbarWidget,
+    required this.barItemWidget,
+  }) : super(key: key);
+
+  @override
+  LayoutTabBuilderState createState() => LayoutTabBuilderState();
+}
+
+class LayoutTabBuilderState extends State<LayoutTabBuilder>
+    with TickerProviderStateMixin {
+  late TabController tabController;
+  late Worker layoutWorker;
+  late Worker screenTabWorker;
+  late ChannelScreenTabController channelScreenTabController;
+  late LayoutController layoutController;
+  List<Widget> tabItems = <Widget>[];
+
+  @override
+  void initState() {
+    super.initState();
+    channelScreenTabController = Get.find<ChannelScreenTabController>(
+        tag: 'channel-screen-${widget.layoutId}');
+
+    layoutController =
+        Get.find<LayoutController>(tag: 'layout${widget.layoutId}');
+    _updateTabItems();
+    _initializeTabController();
+
+    layoutWorker = ever(layoutController.layout, (channels) {
+      setState(() {
+        _updateTabItems();
+        _initializeTabController();
+      });
+    });
+
+    screenTabWorker = ever(channelScreenTabController.tabIndex, (callback) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (tabController.index != callback && tabItems.isNotEmpty && mounted) {
+          tabController.animateTo(callback);
+        }
+      });
+    });
+  }
+
+  void handleTapTabItem(int index) {
+    channelScreenTabController.pageViewIndex.value = index;
+  }
+
+  void _updateTabItems() {
+    tabItems = layoutController.layout
+        .map((e) => widget.barItemWidget(
+              layoutId: widget.layoutId,
+              index: layoutController.layout.indexOf(e),
+              name: e.name,
+            ))
+        .toList();
+  }
+
+  void _initializeTabController() {
+    tabController = TabController(
+      length: tabItems.length,
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    layoutWorker.dispose();
+    screenTabWorker.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.tabbarWidget(
+        tabController: tabController,
+        tabItems: tabItems,
+        onTapTabItem: handleTapTabItem);
+  }
+}
