b14be3b37b8af313fe1a1d632f5292d669c24571
martina <iowoiy.yo@gmail.com>
2023-08-11 21:29:56 +0800

    fix: block4調整

--- packages/shared/lib/widgets/video_block_template/block_4.dart
+++ packages/shared/lib/widgets/video_block_template/block_4.dart
@@ -8,28 +8,44 @@ import '../video_block_grid_view_row.dart';
 
 final logger = Logger();
 
-List<List<Vod>> organizeRowData(List videos) {
-  List<List<Vod>> resultArray = [];
-  List<Vod> tempArray = [];
+List<List<Vod>> organizeRowData(List videos, Blocks block) {
+  List<List<Vod>> result = [];
+  int blockQuantity = block.quantity ?? 0;
+  int videoLength =
+      videos.length > blockQuantity ? blockQuantity : videos.length;
 
-  for (int i = 0; i < videos.length; i++) {
-    tempArray.add(videos[i]);
+  for (int i = 0; i < videoLength;) {
+    if (i != 0 && i == videos.length - 1) break;
     bool hasAreaAd = videos[i].dataType == VideoType.areaAd.index;
-
-    if (hasAreaAd) {
-      resultArray.add(tempArray);
-      tempArray = [];
-    } else if (tempArray.length == 3) {
-      resultArray.add(tempArray);
-      tempArray = [];
+    try {
+      if (hasAreaAd) {
+        result.add([videos[i]]);
+        i++;
+      } else if (i + 2 < videos.length) {
+        // 影片列表
+        result.add([videos[i], videos[i + 1], videos[i + 2]]);
+        i += 3;
+        // logger.i('Block1Widget 有3筆: $i');
+      } else if (i + 1 < videos.length) {
+        // 影片列表
+        result.add([videos[i], videos[i + 1], Vod(0, '')]);
+        i += 2;
+        // logger.i('Block1Widget 有2筆: $i');
+      } else {
+        // 落單的一筆
+        if (i < videos.length) {
+          result.add([videos[i], Vod(0, ''), Vod(0, '')]);
+          i++;
+          // logger.i('Block1Widget 落單的一筆: $i');
+        } else {
+          break;
+        }
+      }
+    } catch (e) {
+      logger.i('err: $e');
     }
   }
-
-  if (tempArray.isNotEmpty) {
-    resultArray.add(tempArray);
-  }
-
-  return resultArray;
+  return result;
 }
 
 // 六直小
@@ -56,7 +72,7 @@ class Block4Widget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<Vod> videos = block.videos?.data ?? [];
-    List<List<Vod>> result = organizeRowData(videos);
+    List<List<Vod>> result = organizeRowData(videos, block);
     return SliverPadding(
       padding: const EdgeInsets.all(8.0),
       sliver: SliverList(
@@ -65,26 +81,27 @@ class Block4Widget extends StatelessWidget {
             if (index == result.length) {
               return buildFooter ?? const SizedBox.shrink();
             }
-            return SizedBox(
-              child: result[index][0].dataType == VideoType.areaAd.index
-                  ? Padding(
-                      padding: const EdgeInsets.only(bottom: 8.0),
-                      child: buildBanner(result[index][0]),
-                    )
-                  : VideoBlockGridViewRow(
-                      videoData: result[index],
-                      imageRatio: BlockImageRatio.block4.ratio,
-                      isEmbeddedAds: block.isEmbeddedAds ?? false,
-                      gridLength: 3,
-                      displayCoverVertical: true,
-                      blockId: block.id ?? 0,
-                      film: film,
-                      displayVideoCollectTimes: false,
-                      displayVideoTimes: false,
-                      displayViewTimes: false,
-                      hasInfoView: false,
-                      buildVideoPreview: buildVideoPreview,
-                    ),
+            return Padding(
+              // padding bottom 8
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: SizedBox(
+                child: result[index][0].dataType == VideoType.areaAd.index
+                    ? buildBanner(result[index][0])
+                    : VideoBlockGridViewRow(
+                        videoData: result[index],
+                        imageRatio: BlockImageRatio.block4.ratio,
+                        isEmbeddedAds: block.isEmbeddedAds ?? false,
+                        gridLength: 3,
+                        displayCoverVertical: true,
+                        blockId: block.id ?? 0,
+                        film: film,
+                        displayVideoCollectTimes: false,
+                        displayVideoTimes: false,
+                        displayViewTimes: false,
+                        hasInfoView: false,
+                        buildVideoPreview: buildVideoPreview,
+                      ),
+              ),
             );
           },
           childCount: result.length + 1,
