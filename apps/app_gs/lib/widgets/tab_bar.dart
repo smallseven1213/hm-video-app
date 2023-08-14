5b5861efe362d33903ac5600139666da36ab269f
martina <iowoiy.yo@gmail.com>
2023-08-14 17:43:12 +0800

    Revert "update: 414~~~~" 沒用
    This reverts commit d541e99bc833915b03025d7e6b3fbdf63998b63d.

--- apps/app_gs/lib/widgets/tab_bar.dart
+++ apps/app_gs/lib/widgets/tab_bar.dart
@@ -20,40 +20,28 @@ class GSTabBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      color: AppColors.colors[ColorKeys.background],
-      padding: padding ?? const EdgeInsets.symmetric(vertical: 15),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Divider(
-              height: 0.5,
-              color: AppColors.colors[AppColors.colors[ColorKeys.primary]]),
-          Align(
-            child: TabBar(
-              isScrollable: true,
-              controller: controller,
-              padding: const EdgeInsets.all(0),
-              labelStyle: const TextStyle(fontSize: 14),
-              unselectedLabelColor: const Color(0xffb2bac5),
-              indicatorSize: TabBarIndicatorSize.label,
-              indicator: UnderlineTabIndicator(
-                borderSide: BorderSide(
-                  width: 3.0,
-                  color: AppColors.colors[ColorKeys.primary]!,
-                ),
+        width: double.infinity,
+        color: AppColors.colors[ColorKeys.background],
+        padding: padding ?? const EdgeInsets.symmetric(vertical: 15),
+        child: Align(
+          child: TabBar(
+            isScrollable: true,
+            controller: controller,
+            padding: const EdgeInsets.all(0),
+            labelStyle: const TextStyle(fontSize: 14),
+            labelPadding:
+                const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            unselectedLabelColor: const Color(0xffb2bac5),
+            indicatorSize: TabBarIndicatorSize.label,
+            indicator: UnderlineTabIndicator(
+              borderSide: BorderSide(
+                width: 3.0,
+                color: AppColors.colors[ColorKeys.primary]!,
               ),
-              tabs: tabs
-                  .map((text) => Tab(height: 18, child: Text(text)))
-                  .toList(),
             ),
+            tabs:
+                tabs.map((text) => Tab(height: 20, child: Text(text))).toList(),
           ),
-          Divider(
-            height: 0.5,
-            color: AppColors.colors[AppColors.colors[ColorKeys.primary]],
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
