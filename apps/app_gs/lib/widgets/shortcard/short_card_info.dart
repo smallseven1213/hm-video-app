c6eae50f0c496c1be15f6e5630780fefa2392d68
martina <iowoiy.yo@gmail.com>
2023-08-14 15:04:16 +0800

    fix: error

--- apps/app_gs/lib/widgets/shortcard/short_card_info.dart
+++ apps/app_gs/lib/widgets/shortcard/short_card_info.dart
@@ -39,73 +39,70 @@ class _ShortCardInfoState extends State<ShortCardInfo> {
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      bottom: 20,
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        padding: const EdgeInsets.only(top: 10, left: 10, right: 10, bottom: 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (widget.data.supplier != null) ...[
-              GestureDetector(
-                onTap: () async {
-                  obsVideoPlayerController.pause();
-                  await MyRouteDelegate.of(context)
-                      .push(AppRoutes.supplier, args: {
-                    'id': widget.data.supplier!.id,
-                  });
-                  obsVideoPlayerController.play();
-                },
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    ActorAvatar(
-                      photoSid: widget.data.supplier!.photoSid,
-                      width: 40,
-                      height: 40,
-                    ),
-                    const SizedBox(width: 8),
-                    const SizedBox(height: 8),
-                    Text(widget.data.supplier!.aliasName ?? '',
-                        style: const TextStyle(
-                          fontSize: 13,
-                          color: Colors.white,
-                        )),
-                    const SizedBox(height: 8),
-                  ],
-                ),
-              )
-            ],
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8),
-              child: Text(
-                widget.title,
-                style: const TextStyle(
-                  fontSize: 15,
-                  color: Colors.white,
-                ),
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      padding: const EdgeInsets.only(top: 10, left: 10, right: 10, bottom: 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (widget.data.supplier != null) ...[
+            GestureDetector(
+              onTap: () async {
+                obsVideoPlayerController.pause();
+                await MyRouteDelegate.of(context)
+                    .push(AppRoutes.supplier, args: {
+                  'id': widget.data.supplier!.id,
+                });
+                obsVideoPlayerController.play();
+              },
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ActorAvatar(
+                    photoSid: widget.data.supplier!.photoSid,
+                    width: 40,
+                    height: 40,
+                  ),
+                  const SizedBox(width: 8),
+                  const SizedBox(height: 8),
+                  Text(widget.data.supplier!.aliasName ?? '',
+                      style: const TextStyle(
+                        fontSize: 13,
+                        color: Colors.white,
+                      )),
+                  const SizedBox(height: 8),
+                ],
+              ),
+            )
+          ],
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: Text(
+              widget.title,
+              style: const TextStyle(
+                fontSize: 15,
+                color: Colors.white,
               ),
             ),
-            if (widget.data.tag.isNotEmpty)
-              Wrap(
-                direction: Axis.horizontal,
-                spacing: 4,
-                runSpacing: 4,
-                children: widget.data.tag
-                    .map((e) => GestureDetector(
-                        onTap: () async {
-                          obsVideoPlayerController.pause();
-                          await MyRouteDelegate.of(context).push(
-                              AppRoutes.supplierTag,
-                              args: {'tagId': e.id, 'tagName': e.name});
-                          obsVideoPlayerController.play();
-                        },
-                        child: ShortCardInfoTag(name: '#${e.name}')))
-                    .toList(),
-              ),
-          ],
-        ),
+          ),
+          if (widget.data.tag.isNotEmpty)
+            Wrap(
+              direction: Axis.horizontal,
+              spacing: 4,
+              runSpacing: 4,
+              children: widget.data.tag
+                  .map((e) => GestureDetector(
+                      onTap: () async {
+                        obsVideoPlayerController.pause();
+                        await MyRouteDelegate.of(context).push(
+                            AppRoutes.supplierTag,
+                            args: {'tagId': e.id, 'tagName': e.name});
+                        obsVideoPlayerController.play();
+                      },
+                      child: ShortCardInfoTag(name: '#${e.name}')))
+                  .toList(),
+            ),
+        ],
       ),
     );
   }
