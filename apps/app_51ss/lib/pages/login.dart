f7e64bd6b78706af5b4810c81d59a4f4ea6621a6
Cyril <smallseven@gmail.com>
2023-08-18 09:46:16 +0800

    修改login scaffold

--- apps/app_51ss/lib/pages/login.dart
+++ apps/app_51ss/lib/pages/login.dart
@@ -5,32 +5,19 @@ import 'package:app_51ss/widgets/forgot_password_button.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:shared/apis/auth_api.dart';
+import 'package:shared/enums/app_routes.dart';
 import 'package:shared/modules/login/login_scaffold.dart';
+import 'package:shared/navigator/delegate.dart';
 
 import '../utils/show_confirm_dialog.dart';
 import '../widgets/auth_text_field.dart';
+import '../widgets/button.dart';
 
 final logger = Logger();
 final authApi = AuthApi();
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
-
-  String? _validateUsername(String? value) {
-    if (value == null || value.isEmpty) {
-      return '請輸入帳號';
-    }
-    // 可在此添加其他驗證邏輯
-    return null;
-  }
-
-  String? _validatePassword(String? value) {
-    if (value == null || value.isEmpty) {
-      return '請輸入密碼';
-    }
-    // 可在此添加其他驗證邏輯
-    return null;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,36 +26,66 @@ class LoginPage extends StatelessWidget {
           title: '登入',
         ),
         body: LoginPageScaffold(
-          showErrorDialog: () {
+          onError: (title, message) {
             showConfirmDialog(
               context: context,
-              title: '登入錯誤',
-              message: '帳號或密碼不正確',
+              title: title,
+              message: message,
               showCancelButton: false,
-              onConfirm: () => {},
+              onConfirm: () {
+                // Navigator.of(context).pop();
+              },
             );
           },
-          child: (accountController, passwordController) {
-            return Column(
-              children: [
-                AuthTextField(
-                  label: '帳號',
-                  controller: accountController,
-                  placeholderText: '請輸入帳號',
-                  validator: _validateUsername,
+          child: (accountController, passwordController, validateUsername,
+                  validatePassword, onLogin) =>
+              SingleChildScrollView(
+                  child: Column(
+            children: [
+              const SizedBox(height: 80),
+              AuthTextField(
+                label: '帳號',
+                controller: accountController,
+                placeholderText: '請輸入帳號',
+                validator: validateUsername,
+              ),
+              const SizedBox(height: 10),
+              AuthTextField(
+                label: '密碼',
+                controller: passwordController,
+                placeholderText: '請輸入密碼',
+                obscureText: true,
+                validator: validateUsername,
+              ),
+              const SizedBox(height: 60),
+              SizedBox(
+                width: 200,
+                child: Button(
+                  text: '登入',
+                  onPressed: () => onLogin(),
                 ),
-                const SizedBox(height: 10),
-                AuthTextField(
-                  label: '密碼',
-                  controller: passwordController,
-                  placeholderText: '請輸入密碼',
-                  obscureText: true,
-                  validator: _validatePassword,
-                ),
-              ],
-            );
-          },
-          forgetPasswordButton: const ForgotPasswordButton(),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      MyRouteDelegate.of(context)
+                          .push(AppRoutes.register, deletePreviousCount: 1);
+                    },
+                    child: const Column(children: [
+                      Text('還沒有帳號',
+                          style: TextStyle(
+                            color: Color(0xFF00B2FF),
+                            decoration: TextDecoration.underline,
+                          )),
+                    ]),
+                  ),
+                  const ForgotPasswordButton()
+                ],
+              ),
+            ],
+          )),
         ));
   }
 }
--- packages/shared/lib/modules/login/login_scaffold.dart
+++ packages/shared/lib/modules/login/login_scaffold.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:shared/enums/app_routes.dart';
 import 'package:shared/navigator/delegate.dart';
 import 'package:shared/apis/auth_api.dart';
 
+import '../../controllers/auth_controller.dart';
+
 final authApi = AuthApi();
 
 class LoginPageScaffold extends StatefulWidget {
-  final Widget forgetPasswordButton;
-  final Function() showErrorDialog;
-  final Widget Function(TextEditingController accountController,
-      TextEditingController passwordController) child;
+  final Function(String? title, String? message)? onError;
+  final Widget Function(
+    TextEditingController accountController,
+    TextEditingController passwordController,
+    String? Function(String? value) validateUsername,
+    String? Function(String? value) validatePassword,
+    Function onLogin,
+  ) child;
   const LoginPageScaffold({
     Key? key,
-    required this.forgetPasswordButton,
-    required this.showErrorDialog,
+    this.onError,
     required this.child,
   }) : super(key: key);
 
@@ -23,45 +29,51 @@ class LoginPageScaffold extends StatefulWidget {
 
 class LoginPageScaffoldState extends State<LoginPageScaffold> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController _accountController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController accountController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  String? validateUsername(String? value) {
+    if (value == null || value.isEmpty) {
+      return '請輸入帳號';
+    }
+    return null;
+  }
+
+  String? validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return '請輸入密碼';
+    }
+    return null;
+  }
+
+  void handleLogin(context) async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        var token = await authApi.login(
+            username: accountController.text,
+            password: passwordController.text);
+        if (token == null) {
+          if (widget.onError != null) {
+            widget.onError!('登入錯誤', '帳號或密碼不正確');
+          }
+        } else {
+          Get.find<AuthController>().setToken(token);
+          MyRouteDelegate.of(context).popRoute();
+        }
+      } catch (error) {
+        if (widget.onError != null) {
+          widget.onError!('登入錯誤', '帳號或密碼不正確');
+        }
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          const SizedBox(height: 80),
-          Form(
-            key: _formKey,
-            child: widget.child(_accountController, _passwordController),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  InkWell(
-                    onTap: () {
-                      MyRouteDelegate.of(context)
-                          .push(AppRoutes.register, deletePreviousCount: 1);
-                    },
-                    child: const Column(children: [
-                      Text('還沒有帳號',
-                          style: TextStyle(
-                            color: Color(0xFF00B2FF),
-                            decoration: TextDecoration.underline,
-                          )),
-                    ]),
-                  ),
-                  widget.forgetPasswordButton,
-                ],
-              ),
-            ],
-          )
-        ],
-      ),
+    return Form(
+      key: _formKey,
+      child: widget.child(accountController, passwordController,
+          validateUsername, validatePassword, handleLogin),
     );
   }
 }
