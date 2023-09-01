4a77e5d07e885c3874ca842e052a5f156b67ba1e
Cyril <smallseven@gmail.com>
2023-09-01 15:33:28 +0800

    style2畫面處理

--- apps/app_gs/lib/screens/main_screen/channel_style_2/index.dart
+++ apps/app_gs/lib/screens/main_screen/channel_style_2/index.dart
@@ -1,18 +1,40 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared/controllers/video_short_by_channel_style2.dart';
 
 import '../../../widgets/base_short_page.dart';
 
-class ChannelStyle2 extends BaseShortPage {
-  ChannelStyle2({
+class ChannelStyle2ShortsPart extends BaseShortPage {
+  ChannelStyle2ShortsPart({
     super.key,
+    required VideoShortByChannelStyle2Controller controller,
   }) : super(
           style: 2,
           createController: () =>
-              Get.put(VideoShortByChannelStyle2Controller()),
+              Get.find<VideoShortByChannelStyle2Controller>(),
           onScrollBeyondFirst: () {
             final controller = Get.find<VideoShortByChannelStyle2Controller>();
             controller.fetchData();
           },
         );
+}
+
+class ChannelStyle2 extends StatelessWidget {
+  ChannelStyle2({Key? key}) : super(key: key);
+
+  final VideoShortByChannelStyle2Controller controller =
+      Get.put(VideoShortByChannelStyle2Controller());
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      if (controller.data.isEmpty) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+
+      return ChannelStyle2ShortsPart(controller: controller);
+    });
+  }
 }
