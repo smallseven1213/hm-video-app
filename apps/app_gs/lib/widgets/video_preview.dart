38fd5131bd067a6aeaadf9f1baf68404b075aed9
Chih.lin <forever040107@yahoo.com.tw>
2023-09-06 12:55:34 +0800

    修正圓角

--- apps/app_gs/lib/widgets/video_preview.dart
+++ apps/app_gs/lib/widgets/video_preview.dart
@@ -17,6 +17,7 @@ import 'video_embedded_ad.dart';
 class ViewInfo extends StatelessWidget {
   final int viewCount;
   final int duration;
+  final bool hasRadius;
   final int? videoCollectTimes;
   final bool? displayVideoCollectTimes;
   final bool? displayVideoTimes;
@@ -26,6 +27,7 @@ class ViewInfo extends StatelessWidget {
     Key? key,
     required this.viewCount,
     required this.duration,
+    required this.hasRadius,
     this.displayVideoCollectTimes = true,
     this.displayVideoTimes = true,
     this.displayViewTimes = true,
@@ -56,6 +58,11 @@ class ViewInfo extends StatelessWidget {
       width: double.infinity,
       padding: const EdgeInsets.all(5),
       decoration: BoxDecoration(
+        borderRadius: hasRadius
+            ? const BorderRadius.vertical(
+                bottom: Radius.circular(10),
+              )
+            : null,
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
@@ -210,12 +217,14 @@ class VideoPreviewWidget extends BaseVideoPreviewWidget {
                   right: 0,
                   bottom: 0,
                   child: ViewInfo(
-                      videoCollectTimes: videoCollectTimes,
-                      viewCount: videoViewTimes ?? 0,
-                      duration: timeLength,
-                      displayVideoTimes: displayVideoTimes,
-                      displayViewTimes: displayViewTimes,
-                      displayVideoCollectTimes: displayVideoCollectTimes)),
+                    videoCollectTimes: videoCollectTimes,
+                    viewCount: videoViewTimes ?? 0,
+                    duration: timeLength,
+                    displayVideoTimes: displayVideoTimes,
+                    displayViewTimes: displayViewTimes,
+                    displayVideoCollectTimes: displayVideoCollectTimes,
+                    hasRadius: hasTitle == true,
+                  )),
           ]),
         ),
         const SizedBox(height: 5),
