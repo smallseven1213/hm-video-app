f8ccf451a185e373bbf179db93b3544c827453c1
martina <iowoiy.yo@gmail.com>
2023-09-11 17:23:06 +0800

    feat: vip page

--- apps/app_ra/lib/pages/vip.dart
+++ apps/app_ra/lib/pages/vip.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shared/models/color_keys.dart';
 import 'package:shared/modules/products/product_consumer.dart';
+import 'package:shared/widgets/sid_image.dart';
 
+import '../config/colors.dart';
 import '../widgets/my_app_bar.dart';
 
 class VipPage extends StatelessWidget {
@@ -22,21 +25,95 @@ class VipPage extends StatelessWidget {
                 itemCount: products.length,
                 itemBuilder: (context, index) {
                   final product = products[index];
-                  return ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(product.photoSid ?? ''),
+
+                  return Container(
+                    padding: const EdgeInsets.all(20),
+                    margin: const EdgeInsets.only(bottom: 8),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: AppColors.colors[ColorKeys.primary]!,
+                      ),
                     ),
-                    title: Text(product.name ?? ''),
-                    subtitle: Text(product.subTitle ?? ''),
-                    trailing: Column(
+                    child: Column(
                       crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
-                        Text('原價: ${product.fiatMoneyPrice}',
-                            style: const TextStyle(color: Colors.white)),
-                        Text('實際售價: ${product.discount}',
-                            style: const TextStyle(color: Colors.white)),
-                        Text('description: ${product.description}',
-                            style: const TextStyle(color: Colors.white)),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipOval(
+                              clipBehavior: Clip.antiAlias,
+                              child: SidImage(
+                                sid: product.photoSid ?? '',
+                                width: 60,
+                                height: 60,
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            Column(
+                              children: [
+                                Text(
+                                  product.name ?? '',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: AppColors
+                                        .colors[ColorKeys.textPrimary]!,
+                                  ),
+                                ),
+                                Text(
+                                  product.subTitle ?? '',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: AppColors
+                                        .colors[ColorKeys.textPrimary]!,
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: Text(
+                                product.description ?? '',
+                                maxLines: 2,
+                                style: TextStyle(
+                                  overflow: TextOverflow.ellipsis,
+                                  fontSize: 12,
+                                  color:
+                                      AppColors.colors[ColorKeys.textPrimary]!,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  '${product.discount}',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.bold,
+                                    color: AppColors
+                                        .colors[ColorKeys.textPrimary]!,
+                                  ),
+                                ),
+                                Text(
+                                  '原價: ${product.fiatMoneyPrice}',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: AppColors
+                                        .colors[ColorKeys.textPrimary]!,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
                       ],
                     ),
                   );
