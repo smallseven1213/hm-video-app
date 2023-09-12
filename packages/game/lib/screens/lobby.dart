ee9d28076d1024139d304ab629bfc2579412fa22
Chih.lin <forever040107@yahoo.com.tw>
2023-09-12 11:03:08 +0800

    移除OrientationBuilder

--- packages/game/lib/screens/lobby.dart
+++ packages/game/lib/screens/lobby.dart
@@ -47,170 +47,159 @@ class CustomFabPosition extends FloatingActionButtonLocation {
 
 class _GameLobbyState extends State<GameLobby>
     with SingleTickerProviderStateMixin {
-  // bool isShowGameList = false;
-  // bool updatedUserInfo = false;
-  // List<GameItem> gameList = [];
-  // List gameHistoryList = [];
-  // bool isShowFab = false;
-  // TabController? _tabController;
+  bool isShowGameList = false;
+  bool updatedUserInfo = false;
+  List<GameItem> gameList = [];
+  List gameHistoryList = [];
+  bool isShowFab = false;
+  TabController? _tabController;
 
-  // UserController get userController => Get.find<UserController>();
-  // GameWalletController gameWalletController = Get.find<GameWalletController>();
+  UserController get userController => Get.find<UserController>();
+  GameWalletController gameWalletController = Get.find<GameWalletController>();
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   _fetchDataInit();
-  //   _tabController = TabController(length: 3, vsync: this);
+  @override
+  void initState() {
+    super.initState();
+    _fetchDataInit();
+    _tabController = TabController(length: 3, vsync: this);
 
-  //   Get.find<AuthController>().token.listen((event) {
-  //     _fetchDataInit();
-  //     userController.fetchUserInfo();
-  //     gameWalletController.fetchWallets();
-  //     logger.i('token changed');
-  //   });
-  // }
+    Get.find<AuthController>().token.listen((event) {
+      _fetchDataInit();
+      userController.fetchUserInfo();
+      gameWalletController.fetchWallets();
+      logger.i('token changed');
+    });
+  }
 
-  // _fetchDataInit() {
-  //   Future.wait([
-  //     GameLobbyApi().registerGame(),
-  //   ]).then((value) {
-  //     GameBannerController();
-  //   });
-  // }
+  _fetchDataInit() {
+    Future.wait([
+      GameLobbyApi().registerGame(),
+    ]).then((value) {
+      GameBannerController();
+    });
+  }
 
-  // @override
-  // void dispose() {
-  //   _tabController!.dispose();
-  //   super.dispose();
-  // }
+  @override
+  void dispose() {
+    _tabController!.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // final gameBannerController = Get.put(GameBannerController());
-    // final gameWalletController = GameWalletController();
-    // final gameConfigController = Get.put(GameConfigController());
-    // final gamesListController = Get.put(GamesListController());
+    final gameBannerController = Get.put(GameBannerController());
+    final gameWalletController = GameWalletController();
+    final gameConfigController = Get.put(GameConfigController());
+    final gamesListController = Get.put(GamesListController());
 
-    return Image.network(
-      'https://images.unsplash.com/photo-1600716051809-e997e11a5d52?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2050&q=80',
-      width: 100,
-      fit: BoxFit.fill,
-    );
-    // Obx(() => gamesListController.isMaintenance.value == true
-    //     ? const GameMaintenance()
-    //     : Scaffold(
-    //         appBar: AppBar(
-    //           leading: Container(),
-    //           backgroundColor: gameLobbyBgColor,
-    //           centerTitle: true,
-    //           title: Text(
-    //             '游戏大厅',
-    //             style: TextStyle(
-    //               fontSize: 18,
-    //               fontWeight: FontWeight.w500,
-    //               color: gameLobbyAppBarTextColor,
-    //             ),
-    //           ),
-    //           // 如果roles是'guest'，就顯示登入按鈕
-    //           actions: userController.info.value.roles.contains('guest')
-    //               ? [
-    //                   InkWell(
-    //                     onTap: () {
-    //                       showModel(
-    //                         context,
-    //                         content: GameLobbyLoginTabs(
-    //                           type: Type.login,
-    //                           onSuccess: () {
-    //                             userController.fetchUserInfo();
-    //                             gameWalletController.mutate();
-    //                             Navigator.pop(context);
-    //                           },
-    //                         ),
-    //                         onClosed: () => Navigator.pop(context),
-    //                       );
-    //                     },
-    //                     child: Padding(
-    //                       padding: const EdgeInsets.all(8),
-    //                       child: Container(
-    //                         width: 85,
-    //                         decoration: BoxDecoration(
-    //                           borderRadius: BorderRadius.circular(24),
-    //                           color: gamePrimaryButtonColor,
-    //                         ),
-    //                         child: Center(
-    //                           child: Text(
-    //                             '註冊/登入',
-    //                             style: TextStyle(
-    //                               color: gamePrimaryButtonTextColor,
-    //                               fontSize: 14,
-    //                               fontWeight: FontWeight.w600,
-    //                             ),
-    //                           ),
-    //                         ),
-    //                       ),
-    //                     ),
-    //                   ),
-    //                 ]
-    //               : null,
-    //         ),
-    //         body: OrientationBuilder(
-    //           builder: (BuildContext context, Orientation orientation) {
-    //             return SafeArea(
-    //               child: Container(
-    //                 color: gameLobbyBgColor,
-    //                 height: double.infinity,
-    //                 padding:
-    //                     const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-    //                 child: Column(
-    //                   children: <Widget>[
-    //                     ClipRRect(
-    //                       borderRadius: BorderRadius.circular(6.0),
-    //                       child: Image.network(
-    //                         'https://images.unsplash.com/photo-1600716051809-e997e11a5d52?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2050&q=80',
-    //                         width: Get.width,
-    //                         fit: BoxFit.fill,
-    //                       ),
-    //                     ),
-    //                     // GameCarousel(data: gameBannerController.gameBanner),
-    //                     // GameMarquee(data: gameBannerController.gameMarquee),
-    //                     GameUserInfo(
-    //                       type: 'lobby',
-    //                       child: Row(
-    //                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //                         children: [
-    //                           // 存款
-    //                           UserInfoDeposit(
-    //                             onTap: () {
-    //                               MyRouteDelegate.of(context).push(
-    //                                 gameConfigController
-    //                                             .switchPaymentPage.value ==
-    //                                         switchPaymentPageType['list']
-    //                                     ? GameAppRoutes.depositList.value
-    //                                     : GameAppRoutes.depositPolling.value,
-    //                               );
-    //                             },
-    //                           ),
-    //                           // 提現
-    //                           UserInfoWithdraw(
-    //                             onTap: () {
-    //                               MyRouteDelegate.of(context).push(
-    //                                 GameAppRoutes.withdraw.value,
-    //                               );
-    //                             },
-    //                           ),
-    //                           // 客服
-    //                           const UserInfoService(),
-    //                         ],
-    //                       ),
-    //                     ),
-    //                     const GameListView(),
-    //                   ],
-    //                 ),
-    //               ),
-    //             );
-    //           },
-    //         ),
-    //       ));
+    return Obx(() => gamesListController.isMaintenance.value == true
+        ? const GameMaintenance()
+        : Scaffold(
+            appBar: AppBar(
+              leading: Container(),
+              backgroundColor: gameLobbyBgColor,
+              centerTitle: true,
+              title: Text(
+                '游戏大厅',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  color: gameLobbyAppBarTextColor,
+                ),
+              ),
+              // 如果roles是'guest'，就顯示登入按鈕
+              actions: userController.info.value.roles.contains('guest')
+                  ? [
+                      InkWell(
+                        onTap: () {
+                          showModel(
+                            context,
+                            content: GameLobbyLoginTabs(
+                              type: Type.login,
+                              onSuccess: () {
+                                userController.fetchUserInfo();
+                                gameWalletController.mutate();
+                                Navigator.pop(context);
+                              },
+                            ),
+                            onClosed: () => Navigator.pop(context),
+                          );
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Container(
+                            width: 85,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(24),
+                              color: gamePrimaryButtonColor,
+                            ),
+                            child: Center(
+                              child: Text(
+                                '註冊/登入',
+                                style: TextStyle(
+                                  color: gamePrimaryButtonTextColor,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ]
+                  : null,
+            ),
+            body: SafeArea(
+              child: Container(
+                color: gameLobbyBgColor,
+                height: double.infinity,
+                padding:
+                    const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                child: Column(
+                  children: <Widget>[
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(6.0),
+                      child: Image.network(
+                        'https://images.unsplash.com/photo-1600716051809-e997e11a5d52?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2050&q=80',
+                        width: Get.width,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    // GameCarousel(data: gameBannerController.gameBanner),
+                    // GameMarquee(data: gameBannerController.gameMarquee),
+                    GameUserInfo(
+                      type: 'lobby',
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          // 存款
+                          UserInfoDeposit(
+                            onTap: () {
+                              MyRouteDelegate.of(context).push(
+                                gameConfigController.switchPaymentPage.value ==
+                                        switchPaymentPageType['list']
+                                    ? GameAppRoutes.depositList.value
+                                    : GameAppRoutes.depositPolling.value,
+                              );
+                            },
+                          ),
+                          // 提現
+                          UserInfoWithdraw(
+                            onTap: () {
+                              MyRouteDelegate.of(context).push(
+                                GameAppRoutes.withdraw.value,
+                              );
+                            },
+                          ),
+                          // 客服
+                          const UserInfoService(),
+                        ],
+                      ),
+                    ),
+                    const GameListView(),
+                  ],
+                ),
+              ),
+            )));
   }
 }
