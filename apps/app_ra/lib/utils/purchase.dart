483e59d2d711c94c91fffb3336f772bcc22cff38
martina <iowoiy.yo@gmail.com>
2023-09-22 10:24:27 +0800

    咻咻

--- apps/app_ra/lib/utils/purchase.dart
+++ apps/app_ra/lib/utils/purchase.dart
@@ -1,12 +1,9 @@
 import 'package:app_ra/utils/show_confirm_dialog.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:shared/apis/vod_api.dart';
-import 'package:shared/controllers/video_detail_controller.dart';
 import 'package:shared/enums/app_routes.dart';
 import 'package:shared/models/hm_api_response.dart';
 import 'package:shared/navigator/delegate.dart';
-import 'package:shared/utils/controller_tag_genarator.dart';
 
 final vodApi = VodApi();
 
@@ -27,7 +24,7 @@ void purchase(
           title: coinNotEnough ? '金幣餘額不足' : '購買失敗',
           message: coinNotEnough ? '請立即前往充值已獲得最完整的體驗' : results.message,
           showCancelButton: coinNotEnough,
-          confirmButtonText: '前往充值',
+          confirmButtonText: coinNotEnough ? '前往充值' : '確認',
           cancelButtonText: '取消',
           onConfirm: () => coinNotEnough
               ? MyRouteDelegate.of(context).push(AppRoutes.coin)
