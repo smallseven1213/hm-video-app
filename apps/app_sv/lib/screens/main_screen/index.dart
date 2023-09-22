7097dbbdd8a5183bfb8cfc874a8682836eb7bf7c
Chih.lin <forever040107@yahoo.com.tw>
2023-09-22 10:35:52 +0800

    fix: 短視頻播放全螢幕時不顯示tab

--- apps/app_sv/lib/screens/main_screen/index.dart
+++ apps/app_sv/lib/screens/main_screen/index.dart
@@ -1,18 +1,23 @@
-import 'package:app_sv/config/colors.dart';
-import 'package:app_sv/screens/main_screen/channels.dart';
-import 'package:app_sv/screens/video/video_player_area/flash_loading.dart';
+import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:shared/controllers/ui_controller.dart';
 import 'package:shared/models/color_keys.dart';
 import 'package:shared/modules/main_layout/display_layout_tab_search_consumer.dart';
 import 'package:shared/modules/main_layout/main_layout_loading_status_consumer.dart';
 import 'package:shared/modules/main_layout/layout_style_tab_bg_consumer.dart';
+
+import 'package:app_sv/config/colors.dart';
+import 'package:app_sv/screens/main_screen/channels.dart';
+import 'package:app_sv/screens/video/video_player_area/flash_loading.dart';
 
 import 'channel_search_bar.dart';
 import 'layout_tab_bar.dart';
 
 class HomeMainScreen extends StatelessWidget {
   final int layoutId;
-  const HomeMainScreen({Key? key, required this.layoutId}) : super(key: key);
+  final uiController = Get.find<UIController>();
+
+  HomeMainScreen({Key? key, required this.layoutId}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -54,18 +59,21 @@ class HomeMainScreen extends StatelessWidget {
                                 )
                               : Container()),
                     ),
-                    LayoutStyleTabBgColorConsumer(
-                      layoutId: layoutId,
-                      child: (({required bool needTabBgColor}) => Container(
-                            color: needTabBgColor
-                                ? AppColors.colors[ColorKeys.primary]
-                                : Colors.transparent,
-                            height: 45,
-                            child: LayoutTabBar(
-                              layoutId: layoutId,
-                            ),
-                          )),
-                    ),
+                    Obx(() => uiController.displayHomeNavigationBar.value
+                        ? LayoutStyleTabBgColorConsumer(
+                            layoutId: layoutId,
+                            child: (({required bool needTabBgColor}) =>
+                                Container(
+                                  color: needTabBgColor
+                                      ? AppColors.colors[ColorKeys.primary]
+                                      : Colors.transparent,
+                                  height: 45,
+                                  child: LayoutTabBar(
+                                    layoutId: layoutId,
+                                  ),
+                                )),
+                          )
+                        : const SizedBox.shrink()),
                   ],
                 )),
               ],
