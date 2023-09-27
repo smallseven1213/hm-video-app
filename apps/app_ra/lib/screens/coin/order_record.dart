6e1a84a7a021c58a8a74f8b8a880851444c02fc7
Chih <chih.lin.hm@gmail.com>
2023-09-27 18:32:53 +0800

    存款記錄改改

--- apps/app_ra/lib/screens/coin/order_record.dart
+++ apps/app_ra/lib/screens/coin/order_record.dart
@@ -54,44 +54,48 @@ class _CustomDropdownMenuState extends State<CustomDropdownMenu> {
       ],
     );
 
-    return InkWell(
-      onTap: () => setState(() => isMenuOpen = !isMenuOpen),
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-        width: 80,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.white, width: 2),
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: isMenuOpen
-            ? Column(
-                children: [
-                  selectedOption,
-                  ...option.map(
-                    (e) => InkWell(
-                      onTap: () => setState(() {
-                        isMenuOpen = !isMenuOpen;
-                        _selectedValue = e['value'];
-                        widget.onChanged!(e['value']!);
-                      }),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8),
-                        child: Text(
-                          e['name'],
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: _selectedValue == e['value']
-                                ? Colors.white
-                                : Colors.grey,
+    return Positioned(
+      top: 0,
+      right: 0,
+      child: InkWell(
+        onTap: () => setState(() => isMenuOpen = !isMenuOpen),
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+          width: 80,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.white, width: 2),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: isMenuOpen
+              ? Column(
+                  children: [
+                    selectedOption,
+                    ...option.map(
+                      (e) => InkWell(
+                        onTap: () => setState(() {
+                          isMenuOpen = !isMenuOpen;
+                          _selectedValue = e['value'];
+                          widget.onChanged!(e['value']!);
+                        }),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                          child: Text(
+                            e['name'],
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: _selectedValue == e['value']
+                                  ? Colors.white
+                                  : Colors.grey,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  )
-                ],
-              )
-            : selectedOption,
+                    )
+                  ],
+                )
+              : selectedOption,
+        ),
       ),
     );
   }
@@ -110,75 +114,83 @@ class _OrderRecordState extends State<OrderRecord> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Align(
-          alignment: Alignment.centerRight,
-          child: CustomDropdownMenu(
-            onChanged: (String? value) {
-              logger.i(value);
-              setState(() {
-                chargeType = value!;
-              });
-            },
-          ),
-        ),
-        const SizedBox(height: 8),
-        Expanded(
-          child: UserOrderRecordConsumer(
-              key: Key('order-record-$chargeType'),
-              type: chargeType,
-              child: (List<Order> records) {
-                if (records.isEmpty) return const NoData();
-                return ListView.builder(
-                  itemCount: records.length,
-                  itemBuilder: (context, index) {
-                    final record = records[index];
-                    return Container(
-                      padding: const EdgeInsets.all(20),
-                      margin: const EdgeInsets.only(bottom: 8),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        border:
-                            Border.all(color: Theme.of(context).primaryColor),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+        Stack(
+          children: [
+            Align(
+              alignment: Alignment.centerRight,
+              child: CustomDropdownMenu(
+                onChanged: (String? value) {
+                  logger.i(value);
+                  setState(() {
+                    chargeType = value!;
+                  });
+                },
+              ),
+            ),
+            const SizedBox(height: 8),
+            Expanded(
+              child: UserOrderRecordConsumer(
+                  key: Key('order-record-$chargeType'),
+                  type: chargeType,
+                  child: (List<Order> records) {
+                    if (records.isEmpty) return const NoData();
+                    return ListView.builder(
+                      itemCount: records.length,
+                      itemBuilder: (context, index) {
+                        final record = records[index];
+                        return Container(
+                          padding: const EdgeInsets.all(20),
+                          margin: const EdgeInsets.only(bottom: 8),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            border: Border.all(
+                                color: Theme.of(context).primaryColor),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Text(
-                                record.product!.name ?? '',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Theme.of(context).primaryColor,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    record.product!.name ?? '',
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Theme.of(context).primaryColor,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                  Text(
+                                    '\$ ${record.orderAmount}',
+                                    style:
+                                        Theme.of(context).textTheme.bodySmall,
+                                  ),
+                                  Text(
+                                    '訂單時間 ${record.createdAt}',
+                                    style:
+                                        Theme.of(context).textTheme.bodySmall,
+                                  ),
+                                  Text(
+                                    '訂單編號 ${record.id}',
+                                    style:
+                                        Theme.of(context).textTheme.bodySmall,
+                                  ),
+                                ],
                               ),
                               Text(
-                                '\$ ${record.orderAmount}',
-                                style: Theme.of(context).textTheme.bodySmall,
-                              ),
-                              Text(
-                                '訂單時間 ${record.createdAt}',
-                                style: Theme.of(context).textTheme.bodySmall,
-                              ),
-                              Text(
-                                '訂單編號 ${record.id}',
-                                style: Theme.of(context).textTheme.bodySmall,
+                                record.paymentStatus == 0 ? '成功' : '失敗',
+                                style:
+                                    Theme.of(context).textTheme.headlineLarge,
                               ),
                             ],
                           ),
-                          Text(
-                            record.paymentStatus == 0 ? '成功' : '失敗',
-                            style: Theme.of(context).textTheme.headlineLarge,
-                          ),
-                        ],
-                      ),
+                        );
+                      },
                     );
-                  },
-                );
-              }),
+                  }),
+            ),
+          ],
         ),
       ],
     );
