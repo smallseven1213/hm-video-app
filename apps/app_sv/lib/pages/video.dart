aacd35af8ce28b102ab45cf03f1147d49c003a34
Chih <chih.lin.hm@gmail.com>
2023-10-04 19:16:37 +0800

    fix-sv: 調整video頁面

--- apps/app_sv/lib/pages/video.dart
+++ apps/app_sv/lib/pages/video.dart
@@ -1,9 +1,15 @@
-import 'package:app_sv/screens/video/index.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:logger/logger.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:shared/apis/user_api.dart';
-
-final logger = Logger();
+import 'package:shared/models/vod.dart';
+import 'package:shared/modules/video/video_provider.dart';
+import 'package:shared/modules/video_player/video_player_provider.dart';
+import 'package:shared/utils/controller_tag_genarator.dart';
+import '../screens/video/nested_tab_bar_view/index.dart';
+import '../screens/video/video_player_area/index.dart';
+import '../screens/video/video_player_area/loading.dart';
+import '../screens/video/video_player_area/purchase_block.dart';
+import '../widgets/wave_loading.dart';
 
 final userApi = UserApi();
 
@@ -18,23 +24,67 @@ class Video extends StatefulWidget {
 
 class VideoState extends State<Video> {
   @override
-  void initState() {
-    super.initState();
-    // setScreenRotation();
-  }
-
-  @override
-  void dispose() {
-    // setScreenPortrait();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return VideoScreen(
-      key: ValueKey(widget.args['id']),
-      id: int.parse(widget.args['id'].toString()),
-      name: widget.args['name'],
+    var id = int.parse(widget.args['id'].toString());
+    final controllerTag = genaratorLongVideoDetailTag(id.toString());
+
+    var name = widget.args['name'];
+    return VideoScreenProvider(
+      id: id,
+      name: name,
+      child: ({
+        required String? videoUrl,
+        required Vod? videoDetail,
+      }) {
+        if (videoUrl == null) {
+          return const WaveLoading();
+        }
+
+        return Scaffold(
+          body: SafeArea(
+            child: Column(
+              children: [
+                VideoPlayerProvider(
+                  key: Key(videoUrl),
+                  tag: videoUrl,
+                  autoPlay: kIsWeb ? false : true,
+                  videoUrl: videoUrl,
+                  videoDetail: videoDetail!,
+                  loadingWidget: AspectRatio(
+                    aspectRatio: 16 / 9,
+                    child: Container(
+                      color: Colors.black,
+                      child: VideoLoading(
+                          coverHorizontal: videoDetail.coverHorizontal ?? ''),
+                    ),
+                  ),
+                  child: (isReady) {
+                    return VideoPlayerArea(
+                      name: name,
+                      videoUrl: videoUrl,
+                      video: videoDetail,
+                      tag: controllerTag,
+                    );
+                  },
+                ),
+                if (videoDetail.isAvailable == false)
+                  PurchaseBlock(
+                    id: id.toString(),
+                    videoDetail: videoDetail,
+                    videoUrl: videoUrl,
+                    tag: controllerTag,
+                  ),
+                Expanded(
+                  child: NestedTabBarView(
+                    videoUrl: videoUrl,
+                    videoDetail: videoDetail,
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
