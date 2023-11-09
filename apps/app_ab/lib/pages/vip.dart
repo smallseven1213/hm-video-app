c8735c147e4f4946c1714be8eb2f76f8518b411c
Chih.lin <forever040107@yahoo.com.tw>
2023-11-09 14:01:50 +0800

    feat-ab: 長視頻付費

---
+++ apps/app_ab/lib/pages/vip.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/custom_app_bar.dart';
+
+class VipPage extends StatelessWidget {
+  const VipPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: CustomAppBar(title: 'VIP'),
+      body: Text('VIP page', style: TextStyle(color: Colors.white)),
+    );
+  }
+}
