5dacc2deab119a82d929126709697680ded0daf0
Cyril <smallseven@gmail.com>
2024-01-02 14:06:47 +0800

    修改付費直播按鈕的顯示邏輯

--- packages/live_ui_basic/lib/widgets/room_payment_button.dart
+++ packages/live_ui_basic/lib/widgets/room_payment_button.dart
@@ -12,38 +12,40 @@ class RoomPaymentButton extends StatelessWidget {
   Widget build(BuildContext context) {
     final LiveRoomController liveroomController = Get.find(tag: pid.toString());
     return Obx(() {
-      if (liveroomController.liveRoom.value.amount == 0) {
+      if (liveroomController.liveRoomInfo.value?.chargeType == 1 ||
+          liveroomController.liveRoom.value.amount > 0) {
         return Container();
+      } else {
+        return GestureDetector(
+          onTap: onTap,
+          child: Container(
+            height: 46.0,
+            decoration: BoxDecoration(
+              color: Color(0xFFae57ff),
+              borderRadius: BorderRadius.circular(23.0),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  "進入付費直播",
+                  style: TextStyle(color: Colors.white),
+                ),
+                const SizedBox(width: 14),
+                Image.asset(
+                  "packages/live_ui_basic/assets/images/rank_diamond.webp",
+                  width: 16,
+                  height: 16,
+                ),
+                const SizedBox(width: 5),
+                Text(liveroomController.liveRoom.value.amount.toString(),
+                    style: const TextStyle(color: Colors.white, fontSize: 12)),
+              ],
+            ),
+          ),
+        );
       }
-      return GestureDetector(
-        onTap: onTap,
-        child: Container(
-          height: 46.0,
-          decoration: BoxDecoration(
-            color: Color(0xFFae57ff),
-            borderRadius: BorderRadius.circular(23.0),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                "進入付費直播",
-                style: TextStyle(color: Colors.white),
-              ),
-              const SizedBox(width: 14),
-              Image.asset(
-                "packages/live_ui_basic/assets/images/rank_diamond.webp",
-                width: 16,
-                height: 16,
-              ),
-              const SizedBox(width: 5),
-              Text(liveroomController.liveRoom.value.amount.toString(),
-                  style: const TextStyle(color: Colors.white, fontSize: 12)),
-            ],
-          ),
-        ),
-      );
     });
   }
 }
