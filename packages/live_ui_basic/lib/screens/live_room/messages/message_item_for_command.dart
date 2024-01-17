5143a9b2ccac69b99ab50be0bc621ff53bbb4242
CyrilTan <smallseven@gmail.com>
2024-01-17 18:41:12 +0800

    MessageForCommand and Gift

---
+++ packages/live_ui_basic/lib/screens/live_room/messages/message_item_for_command.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:live_core/controllers/commands_controller.dart';
+import 'package:live_core/controllers/gifts_controller.dart';
+import 'package:live_core/models/chat_message.dart';
+
+import '../../../libs/format_timestamp.dart';
+
+class MessageItemForCommand extends StatelessWidget {
+  final ChatMessage message;
+
+  const MessageItemForCommand({Key? key, required this.message})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final commandsController = Get.find<CommandsController>();
+    var commandText = commandsController.commands.value
+        .firstWhere((element) => element.id == int.parse(message.objChat.data))
+        .name;
+    var messageText = "${message.objChat.name} 發送指令 : $commandText";
+
+    return ConstrainedBox(
+        constraints: BoxConstraints(
+          maxWidth: MediaQuery.of(context).size.width - 120,
+        ),
+        child: Container(
+          margin: const EdgeInsets.only(top: 10),
+          height: 25,
+          // radius 12.5
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12.5),
+            color: const Color(0x65ae57ff),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ClipOval(
+                  child: Container(
+                      width: 25.0,
+                      height: 25.0,
+                      color: Colors.black,
+                      child: message.objChat.avatar == ""
+                          ? SvgPicture.asset(
+                              'packages/live_ui_basic/assets/svgs/default_avatar.svg',
+                              fit: BoxFit.cover,
+                            )
+                          : // message.objChat.avatar use Image remote
+                          Image.network(
+                              message.objChat.avatar,
+                              fit: BoxFit.cover,
+                            ))),
+              const SizedBox(width: 6),
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: messageText,
+                      style: const TextStyle(color: Colors.white, fontSize: 12),
+                    ),
+                    const WidgetSpan(
+                      child: SizedBox(width: 10), // 提供固定的 10 單位空間
+                    ),
+                    TextSpan(
+                      text: formatTimestamp(message.timestamp),
+                      style: const TextStyle(color: Colors.white, fontSize: 12),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )); // Placeholder for actual UI
+  }
+}
