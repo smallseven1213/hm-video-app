8d3336a700a6ad1d9b36ceaf8ef3dfe2a1f58410
Cyril <smallseven@gmail.com>
2024-02-27 11:45:10 +0800

    animation_layout 3的issues解決

--- packages/live_ui_basic/lib/screens/live_room/center_gift_screen.dart
+++ packages/live_ui_basic/lib/screens/live_room/center_gift_screen.dart
@@ -1,12 +1,24 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:live_core/controllers/chat_result_controller.dart';
 import 'package:live_core/controllers/gifts_controller.dart';
+import 'package:live_core/models/chat_message.dart';
 import 'package:lottie/lottie.dart';
 
-import '../../widgets/x_count.dart';
+class GiftUserData {
+  String userName;
+  String userAvatar;
+  String giftName;
+
+  GiftUserData({
+    required this.userName,
+    required this.userAvatar,
+    required this.giftName,
+  });
+}
 
 class CenterGiftScreen extends StatefulWidget {
   const CenterGiftScreen({
@@ -27,6 +39,9 @@ class CenterGiftScreenState extends State<CenterGiftScreen>
   String? lottiePath;
   Timer? timer;
   ValueNotifier<int> xCount = ValueNotifier<int>(1);
+  ValueNotifier<GiftUserData?> giftUserData =
+      ValueNotifier<GiftUserData?>(null);
+  int animationLayout = 2;
   int currentRepeatCount = 0;
   int targetRepeatCount = 0;
 
@@ -41,7 +56,7 @@ class CenterGiftScreenState extends State<CenterGiftScreen>
         targetRepeatCount = giftMessage.objChat.data.quantity; // 设置目标重复次数
         currentRepeatCount = 0; // 重置当前重复次数
         xCount.value = 1; // 重置xCount
-        setLottieAnimation(giftMessage.objChat.data.gid);
+        setLottieAnimation(giftMessage);
       }
     });
 
@@ -69,16 +84,23 @@ class CenterGiftScreenState extends State<CenterGiftScreen>
     super.dispose();
   }
 
-  void setLottieAnimation(int gid) {
+  void setLottieAnimation(ChatMessage<ChatGiftMessageObjChatData> giftMessage) {
     try {
       isAnimating = true;
+      var gid = giftMessage.objChat.data.gid;
       var gift = giftsController.gifts.value
           .firstWhere((element) => element.id == gid);
       if (gift.animation.isNotEmpty) {
         setState(() {
+          animationLayout = giftMessage.objChat.data.animationLayout;
           lottiePath = gift.animation;
         });
       }
+      giftUserData.value = GiftUserData(
+        userName: giftMessage.objChat.name,
+        userAvatar: giftMessage.objChat.avatar,
+        giftName: gift.name,
+      );
     } catch (e) {
       isAnimating = false;
       setState(() {
@@ -111,6 +133,8 @@ class CenterGiftScreenState extends State<CenterGiftScreen>
               onTap: () {},
               child: Center(
                   child: Lottie.network(
+                height: animationLayout == 2 ? 250 : null,
+                width: animationLayout == 3 ? double.infinity : null,
                 lottiePath ?? "",
                 controller: _lottieController,
                 onLoaded: (composition) {
@@ -122,6 +146,8 @@ class CenterGiftScreenState extends State<CenterGiftScreen>
                 errorBuilder: (context, error, stackTrace) {
                   return Lottie.asset(
                     'packages/live_ui_basic/assets/lotties/present.json',
+                    height: animationLayout == 2 ? 250 : null,
+                    width: animationLayout == 3 ? double.infinity : null,
                     controller: _lottieController,
                     onLoaded: (composition) {
                       var duration = composition.duration;
@@ -133,12 +159,85 @@ class CenterGiftScreenState extends State<CenterGiftScreen>
                 },
               )),
             ),
-            ValueListenableBuilder<int>(
-              valueListenable: xCount,
-              builder: (context, value, child) {
-                return XCountWidget(count: value);
-              },
-            )
+            animationLayout == 2
+                ? SizedBox(
+                    height: 25,
+                    child: ValueListenableBuilder<GiftUserData?>(
+                      valueListenable: giftUserData,
+                      builder: (context, value, child) {
+                        if (value == null) {
+                          return const SizedBox();
+                        }
+                        return Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              height: 25,
+                              decoration: BoxDecoration(
+                                color: const Color(0xffae57ff),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Row(
+                                children: [
+                                  ClipOval(
+                                      child: Container(
+                                          width: 25.0,
+                                          height: 25.0,
+                                          color: Colors.black,
+                                          child: giftUserData.value?.userAvatar
+                                                      .isEmpty ==
+                                                  true
+                                              ? SvgPicture.asset(
+                                                  'packages/live_ui_basic/assets/svgs/default_avatar.svg',
+                                                  fit: BoxFit.cover,
+                                                )
+                                              : Image.network(
+                                                  giftUserData
+                                                      .value!.userAvatar,
+                                                  fit: BoxFit.cover,
+                                                ))),
+                                  const SizedBox(width: 6.7),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 10),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          giftUserData.value?.userName ??
+                                              "某位使用者",
+                                          style: const TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 12),
+                                        ),
+                                        Text(
+                                          '贈送禮物 ${giftUserData.value?.giftName}',
+                                          style: const TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 12),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  const SizedBox(width: 6.7),
+                                ],
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  )
+                : const SizedBox(),
+            // ValueListenableBuilder<int>(
+            //   valueListenable: xCount,
+            //   builder: (context, value, child) {
+            //     return XCountWidget(count: value);
+            //   },
+            // )
           ],
         ),
       ),
