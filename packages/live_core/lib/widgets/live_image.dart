62b0ce184f6fb546864b024a86fbb8a8774c7533
martina <iowoiy.yo@gmail.com>
2024-03-13 15:58:25 +0800

    feat: 直播前台_直播列表 優化 直播間、輪播圖 圖片加載效果

--- packages/live_core/lib/widgets/live_image.dart
+++ packages/live_core/lib/widgets/live_image.dart
@@ -4,23 +4,28 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared/utils/sid_image_result_decode.dart';
 
-class LiveImage extends StatelessWidget {
+class LiveImage extends StatefulWidget {
   final String base64Url;
-
-  // Make sure to define these properties in your class
   final double? width;
   final double? height;
   final BoxFit? fit;
   final Alignment? alignment;
 
-  const LiveImage(
-      {Key? key,
-      required this.base64Url,
-      this.width,
-      this.height,
-      this.fit,
-      this.alignment})
-      : super(key: key);
+  const LiveImage({
+    Key? key,
+    required this.base64Url,
+    this.width,
+    this.height,
+    this.fit,
+    this.alignment,
+  }) : super(key: key);
+
+  @override
+  _LiveImageState createState() => _LiveImageState();
+}
+
+class _LiveImageState extends State<LiveImage> {
+  late Future<ImageProvider<Object>?> _imageProvider;
 
   Future<ImageProvider> _decodeImage(Uint8List file) async {
     await decodeImageFromList(file);
@@ -28,47 +33,46 @@ class LiveImage extends StatelessWidget {
   }
 
   Future<ImageProvider<Object>?> _fetchImageData() async {
-    final response = await http.get(Uri.parse(base64Url));
+    final response = await http.get(Uri.parse(widget.base64Url));
     if (response.statusCode == 200) {
-      var decoded = getSidImageDecode(response
-          .body); // Ensure getSidImageDecode is defined and working correctly
+      var decoded = getSidImageDecode(response.body);
       var file = base64Decode(decoded);
-      var image = await _decodeImage(file);
-      return image;
+      return await _decodeImage(file);
     } else {
       throw Exception('Failed to load image');
     }
   }
 
   @override
+  void initState() {
+    super.initState();
+    _imageProvider = _fetchImageData();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return FutureBuilder<ImageProvider<Object>?>(
-      future: _fetchImageData(),
-      builder: (BuildContext context,
-          AsyncSnapshot<ImageProvider<Object>?> snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const CircularProgressIndicator();
-        } else if (snapshot.hasError) {
-          return Container(
-            color: Colors.grey,
-            child: Text(
-              'Error: ${snapshot.error}',
-              style: const TextStyle(color: Colors.red),
-            ),
-          );
-        } else if (snapshot.hasData) {
-          return Image(
-            image: snapshot.data!,
-            width: width ?? double.infinity,
-            height: height ?? double.infinity,
-            fit: fit ?? BoxFit.cover, // Provide a default value if fit is null
-            alignment: alignment ??
-                Alignment
-                    .center, // Provide a default value if alignment is null
-          );
-        } else {
-          return const Text('No image data');
-        }
+      future: _imageProvider,
+      builder: (context, snapshot) {
+        return AnimatedSwitcher(
+          duration: const Duration(milliseconds: 500),
+          child: snapshot.connectionState == ConnectionState.done &&
+                  snapshot.hasData
+              ? Image(
+                  key: ValueKey(snapshot.data),
+                  image: snapshot.data!,
+                  width: widget.width ?? double.infinity,
+                  height: widget.height ?? double.infinity,
+                  fit: widget.fit ?? BoxFit.cover,
+                  alignment: widget.alignment ?? Alignment.center,
+                )
+              : Container(
+                  width: widget.width,
+                  height: widget.height,
+                  color: const Color(0xFF242a3d),
+                  alignment: Alignment.center,
+                ),
+        );
       },
     );
   }
