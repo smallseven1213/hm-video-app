bc2c252411741131f3f54e2ee4404877adab9dd9
Cyril <smallseven@gmail.com>
2024-04-02 08:42:55 +0800

    fix: #309 語系檔問題

--- apps/app_wl_id1/lib/widgets/auth_text_field.dart
+++ apps/app_wl_id1/lib/widgets/auth_text_field.dart
@@ -28,115 +28,112 @@ class AuthTextFieldState extends State<AuthTextField> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 38,
-              padding: const EdgeInsets.only(top: 2),
-              alignment: Alignment.centerLeft,
-              child: Text(
-                widget.label,
-                style: const TextStyle(
-                  fontSize: 14,
-                  color: Colors.white,
-                  height: 1,
-                ),
+    return SizedBox(
+      height: 90,
+      child: Column(
+        children: [
+          Container(
+            height: 38,
+            padding: const EdgeInsets.only(top: 2),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              widget.label,
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.white,
+                height: 1,
               ),
             ),
-            const SizedBox(width: 8),
-            Expanded(
-                child: Container(
-              height: 60,
-              alignment: Alignment.topLeft,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Focus(
-                    child: TextFormField(
-                      key: widget.key,
-                      autovalidateMode: _autoValidate
-                          ? AutovalidateMode.onUserInteraction
-                          : AutovalidateMode.disabled,
-                      obscureText: widget.obscureText,
-                      validator: widget.validator,
-                      controller: widget.controller,
-                      onChanged: widget.onChanged,
-                      style: const TextStyle(fontSize: 12, color: Colors.white),
-                      textAlign: TextAlign.left,
-                      decoration: InputDecoration(
-                        isDense: true,
-                        hintText: widget.placeholderText,
-                        hintStyle: TextStyle(
-                          color: const Color(0xFFFFFFFF).withOpacity(0.3),
-                        ),
-                        contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 10,
-                          vertical: 10,
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25),
-                          borderSide: const BorderSide(
-                            color: Color(0xFF21AFFF),
-                            width: 1,
-                          ),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25),
-                          borderSide: const BorderSide(
-                            color: Color(0xFF21AFFF),
-                            width: 1,
-                          ),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25),
-                          borderSide: const BorderSide(
-                            color: Color(0xFF21AFFF),
-                            width: 1,
-                          ),
-                        ),
-                        errorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25),
-                          borderSide: const BorderSide(
-                            color: Color(0xFFFF0000),
-                            width: 1,
-                          ),
-                        ),
-                        focusedErrorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25),
-                          borderSide: const BorderSide(
-                            color: Color(0xFFFF0000),
-                            width: 1,
-                          ),
-                        ),
-                        errorStyle: const TextStyle(color: Color(0xFFFF0000)),
-                        suffixIcon: Padding(
-                          padding: const EdgeInsets.only(right: 4),
-                          child: AuthTextFieldSuffixIcon(
-                            controller: widget.controller,
-                          ),
-                        ),
-                        suffixIconConstraints:
-                            const BoxConstraints(minHeight: 15, minWidth: 15),
+          ),
+          const SizedBox(width: 8),
+          Expanded(
+              child: Container(
+            height: 60,
+            alignment: Alignment.topLeft,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Focus(
+                  child: TextFormField(
+                    key: widget.key,
+                    autovalidateMode: _autoValidate
+                        ? AutovalidateMode.onUserInteraction
+                        : AutovalidateMode.disabled,
+                    obscureText: widget.obscureText,
+                    validator: widget.validator,
+                    controller: widget.controller,
+                    onChanged: widget.onChanged,
+                    style: const TextStyle(fontSize: 12, color: Colors.white),
+                    textAlign: TextAlign.left,
+                    decoration: InputDecoration(
+                      isDense: true,
+                      hintText: widget.placeholderText,
+                      hintStyle: TextStyle(
+                        color: const Color(0xFFFFFFFF).withOpacity(0.3),
                       ),
+                      contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 10,
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: const BorderSide(
+                          color: Color(0xFF21AFFF),
+                          width: 1,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: const BorderSide(
+                          color: Color(0xFF21AFFF),
+                          width: 1,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: const BorderSide(
+                          color: Color(0xFF21AFFF),
+                          width: 1,
+                        ),
+                      ),
+                      errorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFFF0000),
+                          width: 1,
+                        ),
+                      ),
+                      focusedErrorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: const BorderSide(
+                          color: Color(0xFFFF0000),
+                          width: 1,
+                        ),
+                      ),
+                      errorStyle: const TextStyle(color: Color(0xFFFF0000)),
+                      suffixIcon: Padding(
+                        padding: const EdgeInsets.only(right: 4),
+                        child: AuthTextFieldSuffixIcon(
+                          controller: widget.controller,
+                        ),
+                      ),
+                      suffixIconConstraints:
+                          const BoxConstraints(minHeight: 15, minWidth: 15),
                     ),
-                    onFocusChange: (hasFocus) {
-                      if (!hasFocus) {
-                        setState(() {
-                          _autoValidate = true;
-                        });
-                      }
-                    },
-                  )
-                ],
-              ),
-            )),
-          ],
-        ),
-      ],
+                  ),
+                  onFocusChange: (hasFocus) {
+                    if (!hasFocus) {
+                      setState(() {
+                        _autoValidate = true;
+                      });
+                    }
+                  },
+                )
+              ],
+            ),
+          ))
+        ],
+      ),
     );
   }
 }
