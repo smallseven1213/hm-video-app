8697c52bb80fe0065215aa41cfc7a40a0adeced2
Cyril <smallseven@gmail.com>
2024-04-23 14:27:14 +0800

    layout2加appbar

--- apps/app_wl_id1/lib/pages/home.dart
+++ apps/app_wl_id1/lib/pages/home.dart
@@ -41,8 +41,11 @@ final screens = {
   HomeNavigatorPathes.layout2: () => MainLayoutBuilder(
         key: Key('layout${layouts[1]}'),
         layoutId: layouts[1],
-        child: HomeMainScreen(
-          layoutId: layouts[1],
+        child: Scaffold(
+          appBar: const SharedAppBar(),
+          body: HomeMainScreen(
+            layoutId: layouts[1],
+          ),
         ),
       ),
   HomeNavigatorPathes.game: () => const Scaffold(
