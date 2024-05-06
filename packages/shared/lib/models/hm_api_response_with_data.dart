3801cd0c1409babf99b902aff40f4225503d13af
Chih.lin <forever040107@yahoo.com.tw>
2024-05-06 11:07:06 +0800

    fix-game: 調整遊戲註冊失敗的error msg from api

--- packages/shared/lib/models/hm_api_response_with_data.dart
+++ packages/shared/lib/models/hm_api_response_with_data.dart
@@ -1,18 +1,25 @@
 class HMApiResponseBaseWithDataWithData<T> {
   String? code;
   T? data;
+  String? message;
 
-  HMApiResponseBaseWithDataWithData({this.code, this.data});
+  HMApiResponseBaseWithDataWithData({
+    this.code,
+    this.data,
+    this.message,
+  });
 
   HMApiResponseBaseWithDataWithData.fromJson(Map<String, dynamic> json) {
     code = json['code'];
     data = json['data'];
+    message = json['message'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['code'] = code;
     data['data'] = data;
+    data['message'] = message;
     return data;
   }
 }
