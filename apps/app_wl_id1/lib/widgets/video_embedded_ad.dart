4e9a518659783b04f6daef5691b4ae8df6ddf495
Cyril <smallseven@gmail.com>
2024-05-06 11:17:38 +0800

    feat: id - 修改video embedded ad不顯示按鈕的部份

--- apps/app_wl_id1/lib/widgets/video_embedded_ad.dart
+++ apps/app_wl_id1/lib/widgets/video_embedded_ad.dart
@@ -18,126 +18,131 @@ class VideoEmbeddedAdWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BannerLink(
-      url: detail.adUrl ?? '',
-      id: detail.id,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          // 廣告封麵
-          AspectRatio(
-            aspectRatio: imageRatio,
-            child: Container(
-              decoration: const BoxDecoration(
-                // borderRadius:
-                //     kIsWeb ? null : BorderRadius.all(Radius.circular(8)),
-                borderRadius: BorderRadius.all(Radius.circular(8)),
-                color: Colors.white,
-              ),
-              foregroundDecoration: BoxDecoration(
-                gradient: kIsWeb
-                    ? null
-                    : LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Colors.black.withOpacity(0.0),
-                          Colors.black.withOpacity(0.3),
-                        ],
-                        stops: const [0.9, 1.0],
-                      ),
-              ),
-              clipBehavior: Clip.antiAlias,
-              child: SidImage(
-                width: double.infinity,
-                height: double.infinity,
-                sid: displayCoverVertical
-                    ? detail.coverVertical ?? ''
-                    : detail.coverHorizontal ?? '',
-                fit: BoxFit.cover,
+    return LayoutBuilder(
+      builder: (context, constraints) => BannerLink(
+        url: detail.adUrl ?? '',
+        id: detail.id,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // 廣告封麵
+            AspectRatio(
+              aspectRatio: imageRatio,
+              child: Container(
+                decoration: const BoxDecoration(
+                  // borderRadius:
+                  //     kIsWeb ? null : BorderRadius.all(Radius.circular(8)),
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  color: Colors.white,
+                ),
+                foregroundDecoration: BoxDecoration(
+                  gradient: kIsWeb
+                      ? null
+                      : LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Colors.black.withOpacity(0.0),
+                            Colors.black.withOpacity(0.3),
+                          ],
+                          stops: const [0.9, 1.0],
+                        ),
+                ),
+                clipBehavior: Clip.antiAlias,
+                child: SidImage(
+                  width: double.infinity,
+                  height: double.infinity,
+                  sid: displayCoverVertical
+                      ? detail.coverVertical ?? ''
+                      : detail.coverHorizontal ?? '',
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 5),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              // App Icon
-              if (imageRatio == 374 / 198) ...[
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(5),
-                  child: SidImage(
-                    sid: detail.appIcon ?? '',
-                    width: 35,
-                    height: 35,
-                  ),
-                ),
-                const SizedBox(width: 10),
-              ],
-              // 主標+副標
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    // 主標
-                    Text(
-                      detail.title,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(
-                        fontSize: 12.0,
-                        color: Colors.white,
-                      ),
+            const SizedBox(height: 5),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // App Icon
+                if (imageRatio == 374 / 198) ...[
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(5),
+                    child: SidImage(
+                      sid: detail.appIcon ?? '',
+                      width: 35,
+                      height: 35,
                     ),
-                    // 副標
-                    Text(
-                      detail.titleSub ?? '',
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(
-                        fontSize: 10,
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              if (imageRatio == 374 / 198) ...[
-                // 下載按鈕
-                const SizedBox(width: 8),
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color(0xffFFC700),
-                    borderRadius: BorderRadius.circular(60),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.yellow.shade700.withOpacity(0.6),
-                        blurRadius: 5,
-                      ),
-                    ], // 圓角大小
                   ),
-                  padding: const EdgeInsets.all(4),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                  const SizedBox(width: 10),
+                ],
+                // 主標+副標
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
                     children: [
-                      const Icon(Icons.download, color: Colors.black, size: 10),
-                      const SizedBox(width: 1),
+                      // 主標
                       Text(
-                        I18n.downloadNow,
+                        detail.title,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
                         style: const TextStyle(
-                          color: Colors.black,
+                          fontSize: 12.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      // 副標
+                      Text(
+                        detail.titleSub ?? '',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
                           fontSize: 10,
+                          color: Colors.grey,
                         ),
                       ),
                     ],
                   ),
                 ),
-              ]
-            ],
-          ),
-        ],
+                if (imageRatio == 374 / 198 &&
+                    constraints.maxWidth >=
+                        MediaQuery.of(context).size.width / 2) ...[
+                  // 下載按鈕
+                  const SizedBox(width: 8),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xffFFC700),
+                      borderRadius: BorderRadius.circular(60),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.yellow.shade700.withOpacity(0.6),
+                          blurRadius: 5,
+                        ),
+                      ], // 圓角大小
+                    ),
+                    padding: const EdgeInsets.all(4),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(Icons.download,
+                            color: Colors.black, size: 10),
+                        const SizedBox(width: 1),
+                        Text(
+                          I18n.downloadNow,
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 10,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ]
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
