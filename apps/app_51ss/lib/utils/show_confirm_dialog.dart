6a92f9d56348bfc53c4b5edef487080ea47cbf34
martina <iowoiy.yo@gmail.com>
2024-07-03 10:06:09 +0800

    feat: android 彈窗提示點選返回時可以把提視窗關掉

--- apps/app_51ss/lib/utils/show_confirm_dialog.dart
+++ apps/app_51ss/lib/utils/show_confirm_dialog.dart
@@ -20,96 +20,99 @@ Future<bool?> showConfirmDialog({
     context: context,
     barrierDismissible: barrierDismissible ?? true,
     builder: (BuildContext context) {
-      return Dialog(
-        backgroundColor: Colors.transparent,
-        insetPadding: const EdgeInsets.all(0),
-        child: IntrinsicHeight(
-          child: Container(
-            constraints: const BoxConstraints(minHeight: 180),
-            width: 280,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(6),
-              color: Colors.white,
-            ),
-            child: Column(
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(top: 20),
-                  height: 30,
-                  child: title != null
-                      ? Text(
-                          title,
-                          style: TextStyle(
-                            color: AppColors.colors[ColorKeys.textPrimary],
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          textAlign: TextAlign.center,
-                        )
-                      : const SizedBox.shrink(),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: SingleChildScrollView(
-                    padding: const EdgeInsets.symmetric(horizontal: 25),
-                    child: message != null
+      return PopScope(
+        canPop: barrierDismissible ?? true,
+        child: Dialog(
+          backgroundColor: Colors.transparent,
+          insetPadding: const EdgeInsets.all(0),
+          child: IntrinsicHeight(
+            child: Container(
+              constraints: const BoxConstraints(minHeight: 180),
+              width: 280,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(6),
+                color: Colors.white,
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    height: 30,
+                    child: title != null
                         ? Text(
-                            message,
+                            title,
                             style: TextStyle(
                               color: AppColors.colors[ColorKeys.textPrimary],
-                              fontSize: 12,
-                              fontWeight: FontWeight.w400,
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
                             ),
                             textAlign: TextAlign.center,
                           )
                         : const SizedBox.shrink(),
                   ),
-                ),
-                // 以下是按鈕區
-                SizedBox(
-                  height: 80,
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        top: 10, bottom: 20, left: 25, right: 25),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        if (showCancelButton)
-                          Expanded(
-                            child: Button(
-                              text: cancelButtonText,
-                              onPressed: () {
-                                if (onCancel != null) {
-                                  onCancel();
-                                }
-                                Navigator.of(context).pop(false);
-                              },
-                              type: 'cancel',
-                              size: 'small',
-                            ),
-                          ),
-                        if (showCancelButton && showConfirmButton)
-                          const SizedBox(width: 10),
-                        if (showConfirmButton)
-                          Expanded(
-                            child: Button(
-                              text: confirmButtonText,
-                              onPressed: () {
-                                if (onConfirm != null) {
-                                  onConfirm();
-                                }
-                                Navigator.of(context).pop(true);
-                              },
-                              type: 'primary',
-                              size: 'small',
-                            ),
-                          ),
-                      ],
+                  Expanded(
+                    flex: 1,
+                    child: SingleChildScrollView(
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
+                      child: message != null
+                          ? Text(
+                              message,
+                              style: TextStyle(
+                                color: AppColors.colors[ColorKeys.textPrimary],
+                                fontSize: 12,
+                                fontWeight: FontWeight.w400,
+                              ),
+                              textAlign: TextAlign.center,
+                            )
+                          : const SizedBox.shrink(),
                     ),
                   ),
-                ),
-              ],
+                  // 以下是按鈕區
+                  SizedBox(
+                    height: 80,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 10, bottom: 20, left: 25, right: 25),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          if (showCancelButton)
+                            Expanded(
+                              child: Button(
+                                text: cancelButtonText,
+                                onPressed: () {
+                                  if (onCancel != null) {
+                                    onCancel();
+                                  }
+                                  Navigator.of(context).pop(false);
+                                },
+                                type: 'cancel',
+                                size: 'small',
+                              ),
+                            ),
+                          if (showCancelButton && showConfirmButton)
+                            const SizedBox(width: 10),
+                          if (showConfirmButton)
+                            Expanded(
+                              child: Button(
+                                text: confirmButtonText,
+                                onPressed: () {
+                                  if (onConfirm != null) {
+                                    onConfirm();
+                                  }
+                                  Navigator.of(context).pop(true);
+                                },
+                                type: 'primary',
+                                size: 'small',
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
