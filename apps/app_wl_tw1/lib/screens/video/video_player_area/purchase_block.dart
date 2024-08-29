17df46aecbdc3d0735c955af391b769d8ae4d916
NickC <nickchen.c.hmi@gmail.com>
2024-08-29 11:43:59 +0800

    feat: #5941 扣點 purchase 修改新 API
    修改成使用UserApi，新增api要帶入PurchaseType，並且各站點共用shared，tw1,tw2,ab,id1已完成修改
    
    Close: #5941

--- apps/app_wl_tw1/lib/screens/video/video_player_area/purchase_block.dart
+++ apps/app_wl_tw1/lib/screens/video/video_player_area/purchase_block.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:app_wl_tw1/utils/purchase.dart';
+import 'package:shared/utils/purchase.dart';
+import 'package:app_wl_tw1/utils/show_confirm_dialog.dart';
 import 'package:get/get.dart';
 import 'package:shared/controllers/video_detail_controller.dart';
 import 'package:shared/enums/charge_type.dart';
@@ -158,6 +159,7 @@ class _PurchaseBlockState extends State<PurchaseBlock> {
                               InkWell(
                                 onTap: () => purchase(
                                   context,
+                                  type: PurchaseType.video,
                                   id: widget.id,
                                   onSuccess: () {
                                     final controllerTag =
@@ -170,6 +172,7 @@ class _PurchaseBlockState extends State<PurchaseBlock> {
                                     videoPlayerInfo.videoPlayerController
                                         ?.play();
                                   },
+                                  showConfirmDialog: showConfirmDialog,
                                 ),
                                 child: Container(
                                   padding: const EdgeInsets.only(
--- apps/app_wl_tw2/lib/screens/video/video_player_area/purchase_block.dart
+++ apps/app_wl_tw2/lib/screens/video/video_player_area/purchase_block.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:app_wl_tw2/utils/purchase.dart';
+import 'package:shared/utils/purchase.dart';
+import 'package:app_wl_tw2/utils/show_confirm_dialog.dart';
 import 'package:get/get.dart';
 import 'package:shared/controllers/video_detail_controller.dart';
 
 import 'package:shared/navigator/delegate.dart';
 import 'package:shared/enums/app_routes.dart';
+import 'package:shared/enums/purchase_type.dart';
 import 'package:shared/models/vod.dart';
 import 'package:shared/modules/video_player/video_player_consumer.dart';
 import 'enums.dart';
+
 
 class PurchaseBlock extends StatefulWidget {
   final Vod videoDetail;
@@ -156,6 +159,7 @@ class _PurchaseBlockState extends State<PurchaseBlock> {
                               ),
                               InkWell(
                                 onTap: () => purchase(context,
+                                    type: PurchaseType.video,
                                     id: int.parse(widget.id.toString()),
                                     onSuccess: () {
                                   final videoDetailController =
@@ -163,7 +167,9 @@ class _PurchaseBlockState extends State<PurchaseBlock> {
                                           tag: widget.tag);
                                   videoDetailController.mutateAll();
                                   videoPlayerInfo.videoPlayerController?.play();
-                                }),
+                                },
+                                showConfirmDialog: showConfirmDialog,
+                                ),
                                 child: Container(
                                   padding: const EdgeInsets.only(
                                     top: 5,
