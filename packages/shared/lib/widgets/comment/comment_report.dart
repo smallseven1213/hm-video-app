f779aed72c271743adda70bbc17d038095d3fc3c
NickC <nickchen.c.hmi@gmail.com>
2024-10-22 15:06:21 +0800

    feat: #6160 舉報功能調整

---
+++ packages/shared/lib/widgets/comment/comment_report.dart
@@ -0,0 +1,280 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shared/localization/shared_localization_delegate.dart';
+import 'package:shared/controllers/comment_controller.dart';
+
+class CommentReport extends StatefulWidget {
+  final int id;
+  final int topicId;
+
+  const CommentReport({
+    Key? key,
+    required this.id,
+    required this.topicId,
+  }) : super(key: key);
+
+  @override
+  CommentReportState createState() => CommentReportState();
+}
+
+class CommentReportState extends State<CommentReport> {
+  String reportTitle = '';
+  int reportType = 0;
+  int reportLength = 0;
+  bool isTypeSelection = true;
+  int selectedIndex = 0;
+  late CommentController commentReportDeleteController;
+  String reason = '';
+  late ScrollController reasonListController;
+  late ScrollController typeListController;
+
+  @override
+  void initState() {
+    super.initState();
+    commentReportDeleteController =
+        Get.find<CommentController>(tag: 'comment-${widget.topicId}');
+    typeListController = ScrollController();
+    reasonListController = ScrollController();
+    initReport();
+  }
+
+  @override
+  void dispose() {
+    typeListController.dispose();
+    reasonListController.dispose();
+    super.dispose();
+  }
+
+  // 舉報初始化
+  void initReport() {
+    reportType = 0;
+    isTypeSelection = true;
+    reportTitle = 'please_select_type';
+    reportLength = commentReportDeleteController.reportList.length;
+    if (typeListController.hasClients) {
+      typeListController.jumpTo(0);
+    } else {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (typeListController.hasClients) {
+          typeListController.jumpTo(0);
+        }
+      });
+    }
+  }
+
+  // 完成舉報類型選擇後
+  void completeTypeSelection(int index) {
+    if (mounted) {
+      setState(() {
+        selectedIndex = 0;
+        reportType = index;
+        isTypeSelection = false;
+        reportTitle = 'please_select_a_reason';
+        reportLength =
+            commentReportDeleteController.reportList[index].options.length;
+        if (reasonListController.hasClients) {
+          reasonListController.jumpTo(0);
+        } else {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            if (reasonListController.hasClients) {
+              reasonListController.jumpTo(0);
+            }
+          });
+        }
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SharedLocalizations localizations = SharedLocalizations.of(context)!;
+    return Container(
+      padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+      height: 350,
+      color: const Color(0xFF333344),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Container(
+              alignment: Alignment.center,
+              height: 4,
+              width: 40,
+              margin: const EdgeInsets.symmetric(vertical: 8),
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(2),
+              ),
+            ),
+          ),
+          const SizedBox(height: 25),
+          Stack(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: IconButton(
+                  onPressed: () {
+                    if (isTypeSelection) {
+                      Navigator.pop(context);
+                    } else {
+                      if (mounted) {
+                        setState(() {
+                          initReport();
+                        });
+                      }
+                    }
+                  },
+                  icon: const Icon(Icons.arrow_back_ios_new),
+                  iconSize: 16,
+                  color: Colors.white,
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  localizations.translate(reportTitle),
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Expanded(
+            // 彈跳視窗內容切換
+            child: isTypeSelection
+                ? _buildTypeSelection(context, localizations)
+                : _buildReasonSelection(context, localizations),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTypeSelection(
+      BuildContext context, SharedLocalizations localizations) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: ListView.builder(
+            controller: typeListController,
+            itemCount: reportLength,
+            itemBuilder: (context, index) {
+              return InkWell(
+                onTap: () {
+                  completeTypeSelection(index);
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: Colors.grey.shade200.withOpacity(0.5),
+                        width: 1.0,
+                      ),
+                    ),
+                  ),
+                  padding: const EdgeInsets.fromLTRB(16, 13, 16, 13),
+                  child: Stack(
+                    children: [
+                      Text(
+                        commentReportDeleteController.reportList[index].title,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                        ),
+                      ),
+                      const Align(
+                        alignment: Alignment.centerRight,
+                        child: Icon(
+                          Icons.arrow_forward_ios,
+                          size: 14,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        const SizedBox(height: 16),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 15),
+          width: double.infinity,
+          height: 55,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildReasonSelection(
+      BuildContext context, SharedLocalizations localizations) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: ListView.builder(
+            controller: reasonListController,
+            itemCount: reportLength,
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.grey.shade200.withOpacity(0.5),
+                      width: 1.0,
+                    ),
+                  ),
+                ),
+                child: RadioListTile(
+                  contentPadding: EdgeInsets.zero,
+                  activeColor: const Color(0xFF6166DC),
+                  value: index,
+                  groupValue: selectedIndex,
+                  onChanged: (int? value) {
+                    setState(() {
+                      selectedIndex = value!;
+                      reason = commentReportDeleteController
+                          .reportList[reportType].options[index];
+                    });
+                  },
+                  title: Text(
+                    commentReportDeleteController
+                        .reportList[reportType].options[index],
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        const SizedBox(height: 16),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xFF6166DC),
+            padding: const EdgeInsets.symmetric(vertical: 15),
+            minimumSize: const Size(double.infinity, 55),
+          ),
+          onPressed: () {
+            commentReportDeleteController.commentReport(
+              widget.id,
+              reportType,
+              reason,
+            );
+            Navigator.pop(context);
+          },
+          child: Text(
+            localizations.translate('i_want_to_report'),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- packages/shared/lib/widgets/comment/comment_report_delete.dart
+++ packages/shared/lib/widgets/comment/comment_report_delete.dart
@@ -3,6 +3,7 @@ import 'package:shared/controllers/user_controller.dart';
 import 'package:get/get.dart';
 import 'package:shared/localization/shared_localization_delegate.dart';
 import 'package:shared/controllers/comment_controller.dart';
+import 'package:shared/widgets/comment/comment_report.dart';
 
 class CommentReportDelete extends StatefulWidget {
   final int id;
@@ -24,15 +25,9 @@ class CommentReportDelete extends StatefulWidget {
 
 class CommentReportDeleteState extends State<CommentReportDelete> {
   final userController = Get.find<UserController>();
-  String reportTitle = '';
-  int reportType = 0;
-  int reportLength = 0;
-  bool isTypeSelection = true;
-  int selectedIndex = 0;
   late CommentController commentReportDeleteController;
   String title = '';
   bool isReport = true;
-  String reason = '';
 
   @override
   void initState() {
@@ -45,30 +40,7 @@ class CommentReportDeleteState extends State<CommentReportDelete> {
       title = 'i_want_to_report';
       isReport = true;
     }
-    initReport();
     super.initState();
-  }
-
-  //舉報初始化
-  void initReport() {
-    reportType = 0;
-    isTypeSelection = true;
-    reportTitle = 'please_select_type';
-    reportLength = commentReportDeleteController.reportList.length;
-  }
-
-  //完成舉報類型選擇後
-  void completeTypeSelection(int index) {
-    if (mounted) {
-      setState(() {
-        selectedIndex = 0;
-        reportType = index;
-        isTypeSelection = false;
-        reportTitle = 'please_select_a_reason';
-        reportLength =
-            commentReportDeleteController.reportList[index].options.length;
-      });
-    }
   }
 
   @override
@@ -97,179 +69,15 @@ class CommentReportDeleteState extends State<CommentReportDelete> {
                 Navigator.pop(context);
                 // 處理舉報邏輯
                 showModalBottomSheet(
-                    context: context,
-                    backgroundColor: const Color(0xFF333344),
-                    builder: (BuildContext context) {
-                      return Container(
-                        padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-                        height: 350,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Center(
-                              child: Container(
-                                alignment: Alignment.center,
-                                height: 4,
-                                width: 40,
-                                margin: const EdgeInsets.symmetric(vertical: 8),
-                                decoration: BoxDecoration(
-                                  color: Colors.grey,
-                                  borderRadius: BorderRadius.circular(2),
-                                ),
-                              ),
-                            ),
-                            const SizedBox(height: 25),
-                            Stack(
-                              children: [
-                                Align(
-                                  alignment: Alignment.centerLeft,
-                                  child: IconButton(
-                                    onPressed: () {
-                                      if (isTypeSelection) {
-                                        Navigator.pop(context);
-                                      } else {
-                                        initReport();
-                                      }
-                                    },
-                                    icon: const Icon(Icons.arrow_back_ios_new),
-                                    iconSize: 16,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                Align(
-                                  alignment: Alignment.center,
-                                  child: Text(
-                                    localizations.translate(reportTitle),
-                                    style: const TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 10),
-                            Expanded(
-                              child: ListView.builder(
-                                itemCount: reportLength,
-                                itemBuilder: (context, index) {
-                                  if (isTypeSelection) {
-                                    return InkWell(
-                                      onTap: () {
-                                        print('$mounted --------dff----');
-                                        completeTypeSelection(index);
-                                      },
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                          border: Border(
-                                            bottom: BorderSide(
-                                              color: Colors.grey.shade200
-                                                  .withOpacity(0.5),
-                                              width: 1.0,
-                                            ),
-                                          ),
-                                        ),
-                                        child: Container(
-                                          padding: const EdgeInsets.fromLTRB(
-                                              16, 13, 16, 13),
-                                          child: Stack(
-                                            children: [
-                                              Text(
-                                                commentReportDeleteController
-                                                    .reportList[index].title,
-                                                style: const TextStyle(
-                                                  color: Colors.white,
-                                                  fontSize: 14,
-                                                ),
-                                              ),
-                                              const Align(
-                                                alignment:
-                                                    Alignment.centerRight,
-                                                child: Icon(
-                                                  Icons.arrow_forward_ios,
-                                                  size: 14,
-                                                  color: Colors.white,
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                    );
-                                  } else {
-                                    return Container(
-                                      decoration: BoxDecoration(
-                                        border: Border(
-                                          bottom: BorderSide(
-                                            color: Colors.grey.shade200
-                                                .withOpacity(0.5),
-                                            width: 1.0,
-                                          ),
-                                        ),
-                                      ),
-                                      child: RadioListTile(
-                                        contentPadding: EdgeInsets.zero,
-                                        activeColor: const Color(0xFF6166DC),
-                                        value: index,
-                                        groupValue: selectedIndex,
-                                        onChanged: (int? value) {
-                                          setState(() {
-                                            selectedIndex = value!;
-                                            reason =
-                                                commentReportDeleteController
-                                                    .reportList[reportType]
-                                                    .options[index];
-                                          });
-                                        },
-                                        title: Text(
-                                          //取得舉報的類型再取得內容
-                                          commentReportDeleteController
-                                              .reportList[reportType]
-                                              .options[index],
-                                          style: const TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 14,
-                                          ),
-                                        ),
-                                      ),
-                                    );
-                                  }
-                                },
-                              ),
-                            ),
-                            const SizedBox(height: 16),
-                            if (!isTypeSelection) ...[
-                              ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: const Color(0xFF6166DC),
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 15),
-                                  minimumSize: const Size(double.infinity, 55),
-                                ),
-                                onPressed: () {
-                                  commentReportDeleteController.commentReport(
-                                    widget.id,
-                                    reportType,
-                                    reason,
-                                  );
-                                  Navigator.pop(context);
-                                },
-                                child: Text(
-                                  localizations.translate('i_want_to_report'),
-                                ),
-                              ),
-                            ] else ...[
-                              Container(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 15),
-                                width: double.infinity,
-                                height: 50,
-                              )
-                            ]
-                          ],
-                        ),
-                      );
-                    });
+                  context: context,
+                  backgroundColor: const Color(0xFF333344),
+                  builder: (BuildContext context) {
+                    return CommentReport(
+                      id: widget.id,
+                      topicId: widget.topicId,
+                    );
+                  },
+                );
               } else {
                 //刪除功能
                 commentReportDeleteController.commentDelete(
