8bca8600c92edb809eace1d8bfc83246c45b6ce8
Cyril <smallseven@gmail.com>
2024-10-25 12:34:17 +0800

    feat(shared): 進行pwa判斷測試

--- packages/shared/lib/utils/platform_utils_web.dart
+++ packages/shared/lib/utils/platform_utils_web.dart
@@ -4,23 +4,14 @@ import 'dart:html' as html;
 import 'package:js/js_util.dart' as js_util;
 
 bool isInStandaloneMode() {
-  // 获取用户代理字符串
-  final userAgent = html.window.navigator.userAgent.toLowerCase();
+  // 檢查 navigator.standalone (iOS PWA)
+  final standalone =
+      js_util.getProperty(html.window.navigator, 'standalone') as bool?;
 
-  // 检测是否为 iOS 设备
-  final isIOS = userAgent.contains('iphone') ||
-      userAgent.contains('ipad') ||
-      userAgent.contains('ipod');
+  // 檢查 display-mode: standalone (其他平台 PWA)
+  final displayModeStandalone =
+      html.window.matchMedia('(display-mode: standalone)').matches;
 
-  if (isIOS) {
-    // 在 iOS 上，通过 navigator.standalone 属性检测
-    final standalone =
-        js_util.getProperty(html.window.navigator, 'standalone') as bool?;
-    return standalone == true;
-  } else {
-    // 在其他平台上，通过 display-mode 媒体查询检测
-    final isStandalone =
-        html.window.matchMedia('(display-mode: standalone)').matches;
-    return isStandalone;
-  }
+  // 只要其中一個為 true 就回傳 true
+  return (standalone == true) || displayModeStandalone;
 }
