1a6ebcb592d346ce106a1db585dedec4b03a9482
Cyril <smallseven@gmail.com>
2024-11-11 11:14:55 +0800

    config(shared): 調整sentry到我要的data

--- packages/shared/lib/modules/video_player/video_player_consumer.dart
+++ packages/shared/lib/modules/video_player/video_player_consumer.dart
@@ -90,38 +90,14 @@ class VideoPlayerConsumerState extends State<VideoPlayerConsumer> {
     ovpController = Get.find<ObservableVideoPlayerController>(tag: widget.tag);
     ovpController.videoPlayerController?.addListener(() {
       if (mounted && ovpController.videoPlayerController!.value.isInitialized) {
+        // Configure scope with tag "video_info"
         Sentry.configureScope((scope) {
-          scope.setTag('category', 'video_info'); // Set the custom tag
+          scope.setTag('category', 'video_info');
         });
-        Sentry.captureEvent(
-          SentryEvent(
-            message: SentryMessage({
-              'videoAction': ovpController.videoAction.value,
-              'isReady': ovpController.isReady.value,
-              'inBuffering':
-                  ovpController.videoPlayerController!.value.isBuffering,
-              'isPlaying': ovpController.videoPlayerController!.value.isPlaying,
-              'videoDurationString': ovpController
-                  .videoPlayerController!.value.duration
-                  .toString()
-                  .split('.')
-                  .first,
-              'videoDuration': ovpController
-                  .videoPlayerController!.value.duration.inSeconds
-                  .toInt(),
-              'videoPositionString': ovpController
-                  .videoPlayerController!.value.position
-                  .toString()
-                  .split('.')
-                  .first,
-              'videoPosition': ovpController
-                  .videoPlayerController!.value.position.inSeconds
-                  .toInt(),
-              'videoSize': ovpController.videoPlayerController!.value.size,
-              'volume': ovpController.videoPlayerController!.value.volume,
-            }.toString()),
-          ),
-        );
+
+        // Capture the entire videoPlayerController value as a readable string
+        Sentry.captureMessage(
+            ovpController.videoPlayerController!.value.toString());
         setState(() {
           videoAction = ovpController.videoAction.value;
           isReady = ovpController.isReady.value;
